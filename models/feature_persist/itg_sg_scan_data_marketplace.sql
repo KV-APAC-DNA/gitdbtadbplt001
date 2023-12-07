0b568b3c9666d14431d90d6d21d0bc00636fc7c3
Akash Pahilwan <apahil01@kenvue.com>
2023-12-07 10:57:20 +0000

    modified incremental strategy

--- models/feature_persist/itg_sg_scan_data_marketplace.sql
+++ models/feature_persist/itg_sg_scan_data_marketplace.sql
@@ -1,7 +1,7 @@
 {{
     config(
         materialized='incremental',
-        incremental_strategy='delet+insert',
+        incremental_strategy='delete+insert',
         unique_key=['trx_date']
     )
 }}
