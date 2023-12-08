6a2ea6e46062f7d3dd118b7e59cb61db3ec70c19
Gary How <whow@kenvue.com>
2023-12-08 04:51:56 +0000

    corrected back to use default schema generation

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,14 +1,3 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
-
-    {%- set default_schema = target.schema -%}
-    {%- if custom_schema_name is none -%}
-
-        {{ default_schema }}
-
-    {%- else -%}
-
-        {{ custom_schema_name | trim }}
-
-    {%- endif -%}
-
+    {{ generate_schema_name_for_env(custom_schema_name, node) }}
 {%- endmacro %}
