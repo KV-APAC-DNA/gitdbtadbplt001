47704257529829b667600fed51ffa3ddd533057b
Akash Pahilwan <apahil01@kenvue.com>
2023-12-21 13:25:01 +0000

    Modified database name as source for currency exchange and wks sap_mara

--- models/asp_regional/aspitg_integration/sap_and_sellin/staging/aspitg_integration__vw_stg_sdl_sap_mara_extract.sql
+++ models/asp_regional/aspitg_integration/sap_and_sellin/staging/aspitg_integration__vw_stg_sdl_sap_mara_extract.sql
@@ -1,10 +1,11 @@
 /*
 This model is an exception for now as data from sap coming directly in wks table.
 Once the cdl table is avaialable the definition of this model will change.
+For now we are hard coding the database and schema name.
 */
 --Import CTE
 with source as (
-    select * from aspwks_integration.wks_sap_mara_extract
+    select * from dev_dna_core.aspwks_integration.wks_sap_mara_extract
 ),
 
 --Logical CTE
