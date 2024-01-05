2a09dad016048b84b71e82ecb24999f365b8f38c
Anjali Agrawal <AAgraw03@kenvue.com>
2024-01-05 10:21:27 +0000

    Updated CDL as source for SDL_SAP_BW_DNA_MATERIAL_BOMLIST

--- models/asp_regional/aspitg_integration/sap_and_sellin/staging/aspitg_integration__vw_stg_sdl_sap_bw_dna_material_bomlist.sql
+++ models/asp_regional/aspitg_integration/sap_and_sellin/staging/aspitg_integration__vw_stg_sdl_sap_bw_dna_material_bomlist.sql
@@ -2,53 +2,53 @@ with
 
 source as (
 
-    select * from {{ source('aspsdl_raw', 'sdl_sap_bw_dna_material_bomlist') }}
+    select * from {{ source('bwa_access', 'bwa_dna_material_bomlist') }}
 
 ),
 
 final as (
 
     select
-        zbomno,
+        bic_zbomno as zbomno,
         plant,
         material,
         component,
         createdon,
         validfrom,
         validto,
-        zvlfromc,
-        zvltoi,
+        bic_zvlfromc as zvlfromc,
+        bic_zvltoi as zvltoi,
         id_credat,
-        zcoqty,
+        bic_zcoqty as zcoqty,
         comp_scrap,
         base_uom,
         recordmode,
-        zhdqty,
+        bic_zhdqty as zhdqty,
         unit,
         matl_type,
-        zmat_type,
+        bic_zmat_type as zmat_type,
         salesorg,
-        zstlan,
-        zstlal,
-        zstlst,
-        zz_tp,
-        zz_cp,
-        zfmnge,
-        zausch,
-        zuposz,
-        zmaktx,
-        zupmng,
-        zlosbs,
-        zlosvn,
-        znlfzt,
-        zverti,
-        znlfzv,
-        znlfmv,
-        zzausch,
-        zavoau,
-        znlfzv1,
-        crt_dttm,
-        updt_dttm
+        bic_zstlan as zstlan,
+        bic_zstlal as zstlal,
+        bic_zstlst as zstlst,
+        bic_zz_tp as zz_tp,
+        bic_zz_cp as zz_cp,
+        bic_zfmnge as zfmnge,
+        bic_zausch as zausch,
+        bic_zuposz as zuposz,
+        bic_zmaktx as zmaktx,
+        bic_zupmng as zupmng,
+        bic_zlosbs as zlosbs,
+        bic_zlosvn as zlosvn,
+        bic_znlfzt as znlfzt,
+        bic_zverti as zverti,
+        bic_znlfzv as znlfzv,
+        bic_znlfmv as znlfmv,
+        bic_zzausch as zzausch,
+        bic_zavoau as zavoau,
+        bic_znlfzv1 as znlfzv1,
+        current_timestamp()::timestamp_ntz(9) as crt_dttm,
+        current_timestamp()::timestamp_ntz(9) as updt_dttm
 
     from source
 
