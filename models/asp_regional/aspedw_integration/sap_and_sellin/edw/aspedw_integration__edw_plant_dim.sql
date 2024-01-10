0c88bf2a0fa0203490e8b22a9cf25d41f7a858ff
Anjali Agrawal <AAgraw03@kenvue.com>
2024-01-10 10:35:58 +0000

    Type casted the columns of varchar type to specified length

--- models/asp_regional/aspedw_integration/sap_and_sellin/edw/aspedw_integration__edw_plant_dim.sql
+++ models/asp_regional/aspedw_integration/sap_and_sellin/edw/aspedw_integration__edw_plant_dim.sql
@@ -17,15 +17,15 @@ source as (
 
 final as (
     select
-        plnt,
-        plnt_nm,
-        ctry as ctry_key,
-        src_sys,
-        prchsng_org,
-        rgn,
-        co_cd,
-        fctry_cal,
-        mkt_clus,
+        plnt :: varchar(4) as plnt,
+        plnt_nm :: varchar(40) as plnt_nm,
+        ctry :: varchar(3) as ctry_key,
+        src_sys :: varchar(10) as src_sys,
+        prchsng_org :: varchar(4) as prchsng_org,
+        rgn :: varchar(3) as rgn,
+        co_cd :: varchar(4) as co_cd,
+        fctry_cal :: varchar(2) as fctry_cal,
+        mkt_clus :: varchar(2) as mkt_clus,
         current_timestamp()::timestamp_ntz(9) as crt_dttm,
         current_timestamp()::timestamp_ntz(9) as updt_dttm
     from source
