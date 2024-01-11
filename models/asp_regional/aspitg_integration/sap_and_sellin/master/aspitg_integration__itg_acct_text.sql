3817e1df26d53b57282648b8c2cbe09844dec86e
Anjali Agrawal <AAgraw03@kenvue.com>
2024-01-11 05:38:24 +0000

    updated size for txtsh

--- models/asp_regional/aspitg_integration/sap_and_sellin/master/aspitg_integration__itg_acct_text.sql
+++ models/asp_regional/aspitg_integration/sap_and_sellin/master/aspitg_integration__itg_acct_text.sql
@@ -20,7 +20,7 @@ final as (
     chrt_accts :: varchar(4) as chrt_acct,
     account :: varchar(10) as acct_num,
     langu :: varchar(2) as lang_key,
-    txtsh :: varchar(5000) as shrt_desc,
+    txtsh :: varchar(100) as shrt_desc,
     txtmd :: varchar(5000) as med_desc,
     current_timestamp()::timestamp_ntz(9) as crt_dttm,
     current_timestamp()::timestamp_ntz(9) as updt_dttm
