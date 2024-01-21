ccecf8354918f28af076adf74da79d2f51a0b8f3
Anjali Agrawal <AAgraw03@kenvue.com>
2024-01-21 06:35:30 +0000

    Created model itg_rg_travel_retail_channel_mapping

---
+++ models/asp_regional/aspitg_integration/mds/master/aspitg_integration__itg_rg_travel_retail_channel_mapping.sql
@@ -0,0 +1,26 @@
+--Import CTE
+with source as (
+    select * from {{ source('aspsdl_raw', 'sdl_mds_apac_dcl_customers') }}
+),
+
+--Logical CTE
+
+--Final CTE
+final as (
+    select 
+        trim (sales_channel_name) :: varchar(100) as ales_channel,
+        trim(sales_location_name) :: varchar(100) as sales_location,
+        trim(cust_num) :: varchar(30) as cust_num,
+        trim(name) :: varchar(255) as cust_nm,
+        upper(trim(ctry_key_code)) :: varchar(5) as ctry_cd,
+        trim(ctry_key_name) :: varchar(30) as country_name,
+        upper(trim(retailer_name_code)) :: varchar(100) as retailer_name,
+        upper(trim(location_name)) :: varchar(100) as door_name,
+        trim(remarks) :: varchar(100) as remarks,
+        trim(enterdatetime) :: timestamp_ntz(9) as crt_dttm,
+        current_timestamp() :: timestamp_ntz(9) as updt_dttm
+    from source
+)
+
+--Final select
+select * from final
