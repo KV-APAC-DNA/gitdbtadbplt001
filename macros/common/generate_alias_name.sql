81ef0b8833f0cb3d9b6ea8e8e563a6f4a701c293
Akash Pahilwan <apahil01@kenvue.com>
2024-02-08 06:19:21 +0000

    Added DQ checks for Flat files

---
+++ macros/common/generate_alias_name.sql
@@ -0,0 +1,11 @@
+{% macro generate_alias_name(custom_alias_name=none, node=none) -%}
+    {%- if custom_alias_name and node.schema.lower().endswith('workspace') -%}
+        {{ node.name }}
+    {%- elif custom_alias_name -%}
+        {{ custom_alias_name | trim }}
+    {%- elif node.version -%}
+        {{ return(node.name ~ "_v" ~ (node.version | replace(".", "_"))) }}
+    {%- else -%}
+        {{ node.name }}
+    {%- endif -%}
+{%- endmacro %}
