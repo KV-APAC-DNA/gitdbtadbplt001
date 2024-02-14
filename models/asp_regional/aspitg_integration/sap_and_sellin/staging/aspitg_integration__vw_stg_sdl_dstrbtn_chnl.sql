5dec1fc2926d40819d43deb5c5dba21a91684988
Anjali Agrawal <AAgraw03@kenvue.com>
2024-02-14 07:07:26 +0000

    Converted nulls to blanks for model dstrbtn_chnl

--- models/asp_regional/aspitg_integration/sap_and_sellin/staging/aspitg_integration__vw_stg_sdl_dstrbtn_chnl.sql
+++ models/asp_regional/aspitg_integration/sap_and_sellin/staging/aspitg_integration__vw_stg_sdl_dstrbtn_chnl.sql
@@ -3,15 +3,15 @@ with source as (
     select * from {{ source('bwa_access', 'bwa_distribution_channel_text') }}
 ),
 
---Logical CTE
-
 --Final CTE
 final as (
     select 
-        distr_chan,
-        langu,
-        txtsh,
-        txtmd 
+        coalesce(distr_chan,'') as distr_chan,
+        coalesce(langu,'') as langu,
+        coalesce(txtsh,'') as txtsh,
+        coalesce(txtmd,'') as txtmd,
+        current_timestamp()::timestamp_ntz(9) as crt_dttm,
+        current_timestamp()::timestamp_ntz(9) as updt_dttm
     from source
 )
 
