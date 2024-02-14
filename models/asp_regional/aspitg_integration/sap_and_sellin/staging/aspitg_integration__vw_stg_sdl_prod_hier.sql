246db64b5f6d515f7200515d6f6a1faf74678f8b
Anjali Agrawal <AAgraw03@kenvue.com>
2024-02-14 07:28:48 +0000

    Added the etl columns and added coalesce

--- models/asp_regional/aspitg_integration/sap_and_sellin/staging/aspitg_integration__vw_stg_sdl_prod_hier.sql
+++ models/asp_regional/aspitg_integration/sap_and_sellin/staging/aspitg_integration__vw_stg_sdl_prod_hier.sql
@@ -8,9 +8,11 @@ with source as (
 --Final CTE
 final as (
     select 
-        prod_hier,
-        langu,
-        replace(replace(replace(txtmd,'#',''),'*',''),',','') as txtmd
+        coalesce(prod_hier,'') as prod_hier,
+        coalesce(langu,'') as langu,
+        replace(replace(replace(txtmd,'#',''),'*',''),',','') as txtmd,
+        current_timestamp()::timestamp_ntz(9) as crt_dttm,
+        current_timestamp()::timestamp_ntz(9) as updt_dttm
     from source
 )
 
