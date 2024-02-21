8c60077e9cd4d139d554d18e3d32a3302c04c074
Akash Pahilwan <apahil01@kenvue.com>
2024-02-21 13:54:47 +0000

    Modified ECC Account text View

--- models/asp_regional/aspitg_integration/sap_and_sellin/staging/aspitg_integration__vw_stg_sdl_sap_ecc_account_text.sql
+++ models/asp_regional/aspitg_integration/sap_and_sellin/staging/aspitg_integration__vw_stg_sdl_sap_ecc_account_text.sql
@@ -6,7 +6,7 @@ source as (
 
 ),
 
-final as (
+without_pipe as (
 
     select
         chrt_accts,
@@ -17,6 +17,25 @@ final as (
         current_timestamp()::timestamp_ntz(9) as crt_dttm,
         current_timestamp()::timestamp_ntz(9) as updt_dttm
     from source
+    where chrt_accts not like '%|%'
+),
+with_pipe as (
+    select
+        trim(split(chrt_accts,'|')[0],'"') as chrt_accts,
+        trim(split(chrt_accts,'|')[1],'"') as account,
+        trim(split(chrt_accts,'|')[2],'"') as langu, 
+        replace(trim(trim(split(chrt_accts,'|')[3],'"')),'–','') as txtsh,
+        replace(trim(trim(split(chrt_accts,'|')[4],'"')),'–','') as txtmd,
+        current_timestamp()::timestamp_ntz(9) as crt_dttm,
+        current_timestamp()::timestamp_ntz(9) as updt_dttm
+    from source
+    where chrt_accts like '%|%'
+),
+final as (
+select * from without_pipe
+union all 
+select * from with_pipe
 )
 
 select * from final
+
