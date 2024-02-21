ae75a65f382da995a524f124f905a9c6fefd02cd
Niketh Narasimhan <nnaras01@kenvue.com>
2024-02-21 09:13:39 +0000

    Added cust_hier and Geo_hier

--- models/asp_regional/aspitg_integration/sap_and_sellin/staging/aspitg_integration__vw_stg_sdl_gcgh_geo_hier.sql
+++ models/asp_regional/aspitg_integration/sap_and_sellin/staging/aspitg_integration__vw_stg_sdl_gcgh_geo_hier.sql
@@ -11,7 +11,7 @@ final as (
     region_name::varchar(50) as region ,
     cluster1_name::varchar(30) as cluster ,
     subcluster_name::varchar(30) as subcluster ,
-    gcgh_country_mst_name::varchar(30) as market 
+    gcgh_country_mst_name::varchar(30) as market ,
     case gcgh_country_mst_name
     WHEN 'Australia' THEN 'AU'
     WHEN 'Bangladesh' THEN 'BD'
@@ -90,12 +90,11 @@ final as (
     WHEN 'Tuvalu' THEN 'TUV'
     WHEN 'Vanuatu' THEN 'VUT'
     WHEN 'Vietnam' THEN 'VNM'
-  END AS country_code_iso3
-    -- country_code_iso3::varchar(10) as country_code_iso3 ,
-    -- market_type::varchar(30) as market_type ,
-    -- cdl_datetime::varchar(30) as cdl_datetime ,
-    -- cdl_source_file::varchar(50) as cdl_source_file ,
-    -- load_key::varchar(100) as load_key 
+  END AS country_code_iso3 ,
+  null as market_type ,
+  null as cdl_datetime ,
+  null as cdl_source_file ,
+  null as load_key 
     from source
 )
 
