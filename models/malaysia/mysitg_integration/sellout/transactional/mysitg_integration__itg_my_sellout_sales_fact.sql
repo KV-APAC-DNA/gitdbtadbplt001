5b73bafbce199efd7752b73a744d09a145c4f749
Anjali Agrawal <aagrawal@mycervello.com>
2024-02-27 13:57:13 +0530

    Recovering Sellout sales and stock files

--- models/malaysia/mysitg_integration/sellout/transactional/mysitg_integration__itg_my_sellout_sales_fact.sql
+++ models/malaysia/mysitg_integration/sellout/transactional/mysitg_integration__itg_my_sellout_sales_fact.sql
@@ -5,16 +5,17 @@
         unique_key=["dstrbtr_id","sls_ord_dt"]
     )
 }}
+{% set cte_to_execute = 'my_joint_monthly' %}  
+
+{% if cte_to_execute == 'my_joint_monthly' %}
 
 with source as (
-    select * from {{ source('myssdl_raw', 'sdl_my_monthly_sellout_sales_fact') }}
+  select * from {{ source('myssdl_raw', 'sdl_my_monthly_sellout_sales_fact') }}
 ),
-
 imier as (
-    select * from {{ ref('mysitg_integration__itg_my_ids_exchg_rate') }}
+  select * from {{ ref('mysitg_integration__itg_my_ids_exchg_rate') }}
 ),
-
-logical as (
+union1 as (
     select
         dstrbtr_id,
         sls_ord_num,
@@ -48,9 +49,145 @@ logical as (
     from source, imier
     where imier.cust_id(+)=source.dstrbtr_id
         and imier.yearmo(+)=replace(substring(to_date(sls_ord_dt,'dd.mm.yyyy'), 0, 7),'-', '')
+)
+
+select * from union1
+
+{% elif cte_to_execute == 'my_sellout_sales' %}
+
+with source as (
+  select * from {{ source('myssdl_raw', 'sdl_my_monthly_sellout_sales_fact') }}
+),
+imier as (
+  select * from {{ ref('mysitg_integration__itg_my_ids_exchg_rate') }}
+),
+wks_my_sellout_sales_fact as(
+  select * from {{ ref('myswks_integration__wks_my_sellout_sales_fact') }}
+),
+itg_my_material_dim as (
+    select * from {{ ref('mysitg_integration__itg_my_material_dim') }}
+),
+itg_my_material_map as (
+    select  * from {{ ref('mysitg_integration__itg_my_material_map') }}
+),
+union2 as (
+   select
+    dstrbtr_id,
+    sls_ord_num,
+    to_date(sls_ord_dt, 'DD/MM/YYYY') as sls_ord_dt,
+    type,
+    cust_cd,
+    dstrbtr_wh_id,
+    item_cd,
+    dstrbtr_prod_cd,
+    ean_num,
+    dstrbtr_prod_desc,
+    cast(replace(grs_prc,',','') as decimal(20, 4)) as grs_prc,
+    cast(replace(qty,',','') as decimal(20, 4)) as qty,
+    uom,
+    cast(replace(qty_pc,',','') as decimal(20, 4)) as qty_pc,
+    cast(replace(qty_aft_conv,',','') as decimal(20, 4)) as qty_aft_conv,
+    cast(replace(subtotal_1,',','') as decimal(20, 4)) * coalesce(t2.exchng_rate, 1) as subtotal_1,
+    cast(replace(discount,',','') as decimal(20, 4)) * coalesce(t2.exchng_rate, 1) as discount,
+    cast(replace(subtotal_2,',','') as decimal(20, 4)) * coalesce(t2.exchng_rate, 1) as subtotal_2,
+    cast(replace(bottom_line_dscnt,',','') as decimal(20, 4)) * coalesce(t2.exchng_rate, 1) as bottom_line_dscnt,
+    cast(replace(total_amt_aft_tax,',','') as decimal(20, 4)) * coalesce(t2.exchng_rate, 1) as total_amt_aft_tax,
+    cast(replace(total_amt_bfr_tax,',','') as decimal(20, 4)) * coalesce(t2.exchng_rate, 1) as total_amt_bfr_tax,
+    sls_emp,
+    custom_field1,
+    custom_field2,
+    custom_field3 as sap_matl_num,
+    filename,
+    t1.cdl_dttm as cdl_dttm,
+    t1.curr_dt as crtd_dttm,
+    current_timestamp() as updt_dttm
+    from wks_my_sellout_sales_fact as t1, imier as t2
+    where
+    t2.cust_id(+) = t1.dstrbtr_id
+    and t2.yearmo(+) = substring(replace(to_date(sls_ord_dt, 'DD/MM/YYYY'), '-', ''), 0, 7)
+),
+immd as (
+  select
+    item_bar_cd,
+    item_cd,
+    status
+  from (
+    select
+      item_bar_cd,
+      item_cd,
+      status,
+      case
+        when status = 'ACTIVE'
+        then 'A' || item_bar_cd
+        when status = 'INACTIVE'
+        then 'B' || item_bar_cd
+        when status = 'DISCON'
+        then 'C' || item_bar_cd
+      end as flag,
+      row_number() over (partition by item_bar_cd order by flag asc) as row_count
+    from (
+      select distinct
+        item_bar_cd,
+        item_cd,
+        status
+      from (
+        select
+          item_bar_cd,
+          min(item_cd) over (partition by item_bar_cd, status) as item_cd, /* casting on item_cd */
+          status
+        from itg_my_material_dim
+      )
+    )
+  ) as t3
+  where
+    t3.row_count = 1
+),
+a as (
+    select immm.item_cd,
+             immd.item_bar_cd,
+             immm.ext_item_cd
+      from itg_my_material_map immm,
+           itg_my_material_dim immd
+      where ltrim(immm.item_cd,'0') = ltrim(immd.item_cd,'0')
+      qualify row_number() over (partition by immm.ext_item_cd order by null) = 1
+      
 ),
 
-final as (
+transformed as (
+    select  
+    union2.dstrbtr_id,
+    union2.sls_ord_num,
+    union2.sls_ord_dt AS sls_ord_dt,
+    union2.type,
+    union2.cust_cd,
+    union2.dstrbtr_wh_id,
+    union2.item_cd,
+    union2.dstrbtr_prod_cd,
+    union2.ean_num,
+    union2.dstrbtr_prod_desc,
+    union2.grs_prc,
+    union2.qty,
+    union2.uom,
+    union2.qty_pc,
+    union2.qty_aft_conv,
+    union2.subtotal_1,
+    union2.discount,
+    union2.subtotal_2,
+    union2.bottom_line_dscnt,
+    union2.total_amt_aft_tax,
+    union2.total_amt_bfr_tax,
+    union2.sls_emp,
+    union2.custom_field1,
+    union2.custom_field2,
+    nvl(nvl(union2.SAP_MATL_NUM, immd.item_cd), a.item_cd) as sap_matl_num,
+    union2.filename,
+    union2.cdl_dttm,
+    union2.crtd_dttm,
+    union2.updt_dttm from union2,immd,a 
+    where ltrim(immd.item_bar_cd(+),'0') = ltrim(ean_num,'0')
+    and ltrim(a.ext_item_cd(+),'0') = ltrim(dstrbtr_prod_cd,'0') 
+),
+final as(
     select 
         dstrbtr_id::varchar(50) as dstrbtr_id,
         sls_ord_num::varchar(50) as sls_ord_num,
@@ -81,7 +218,8 @@ final as (
         cdl_dttm::varchar(255) as cdl_dttm,
         crtd_dttm::timestamp_ntz(9) as crtd_dttm,
         updt_dttm::timestamp_ntz(9) as updt_dttm
-    from logical
+        from transformed
 )
-
 select * from final
+{% endif %}
+
