94a0c6ea6a32f79761b83f4d8fd95212e5522cb5
Niketh Narasimhan <nnaras01@kenvue.com>
2024-03-08 07:27:55 +0000

    folder structure

--- models/thailand/thaedw_integration/market_share/transaction/thaedw_integration__edw_th_market_share_distribution_analysis.sql
+++ models/thailand/thaedw_integration/market_share/transaction/thaedw_integration__edw_th_market_share_distribution_analysis.sql
@@ -1,8 +1,8 @@
-with EDW_VW_TH_MARKET_SHARE_DISTRIBUTION as 
+with edw_vw_th_market_share_distribution as 
  (select * from {{ ref('thaedw_integration__edw_vw_th_market_share_distribution') }}
 ),
 final as (
-SELECT 
+select 
 cntry_cd::varchar(2) as cntry_cd,
 cntry_nm::varchar(8) as cntry_nm,
 chnl::varchar(11) as chnl,
@@ -11,5 +11,5 @@ yr_mnth::number(18,0) as yr_mnth,
 measure::varchar(100) as measure,
 category::varchar(256) as category,
 value::number(20,5) as value
-FROM EDW_VW_TH_MARKET_SHARE_DISTRIBUTION)
+from edw_vw_th_market_share_distribution)
 select * from final
