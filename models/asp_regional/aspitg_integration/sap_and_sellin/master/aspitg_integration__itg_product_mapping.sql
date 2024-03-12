37558ed13f11e48870e0955b92054dd5f1d0b4f9
Akash Pahilwan <apahil01@kenvue.com>
2024-03-12 08:02:51 +0000

    Added model itg_product_mapping

---
+++ models/asp_regional/aspitg_integration/sap_and_sellin/master/aspitg_integration__itg_product_mapping.sql
@@ -0,0 +1,21 @@
+
+--Import CTE
+with source as (
+    select * from {{ ref('aspwks_integration__wks_itg_prod_hier') }}
+),
+
+--Logical CTE
+
+--Final CTE
+final as (
+    select
+        prod_hier::varchar(18) as prod_hier,
+        langu::varchar(1) as langu,
+        txtmd::varchar(40) as txtmd,
+        current_timestamp()::timestamp_ntz(9) as crt_dttm,
+        current_timestamp()::timestamp_ntz(9) as updt_dttm
+  from source
+)
+
+--Final select
+select * from final
