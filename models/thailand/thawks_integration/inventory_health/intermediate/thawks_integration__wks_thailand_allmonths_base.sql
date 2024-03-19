f8d6a2fc8592d1829a236320888ab8e525ce0551
Sathya M <SM05@kenvue.com>
2024-03-19 06:26:33 +0000

    Updated dq checks

---
+++ models/thailand/thawks_integration/inventory_health/intermediate/thawks_integration__wks_thailand_allmonths_base.sql
@@ -0,0 +1,59 @@
+with edw_vw_os_time_dim as(
+select * from {{ ref('sgpedw_integration__edw_vw_os_time_dim') }}
+),
+wks_thailand_base as (
+select * from dev_dna_core.snaposewks_integration.wks_thailand_base
+),
+all_months as (
+  select distinct
+    dstr_nm,
+    sap_parent_customer_key,
+    sap_parent_customer_desc,
+    matl_num,
+    b.mnth_id
+  from (
+    select distinct
+      dstr_nm,
+      sap_prnt_cust_key as sap_parent_customer_key,
+      sap_prnt_cust_desc as sap_parent_customer_desc,
+      sku_cd as matl_num
+    from wks_thailand_base
+  ) as a, (
+    select distinct
+      cal_year as year,
+      cal_mnth_id as mnth_id
+    from edw_vw_os_time_dim /* limit 100 */
+    where
+      cal_year >= (
+        date_part(year, current_timestamp()) - 6
+      )
+  ) as b
+),
+transformed as(
+select
+  all_months.dstr_nm,
+  all_months.sap_parent_customer_key,
+  all_months.sap_parent_customer_desc,
+  all_months.matl_num,
+  all_months.mnth_id as month,
+  sum(b.so_sls_qty) as so_qty,
+  sum(b.so_grs_trd_sls) as so_value,
+  sum(b.inventory_quantity) as inv_qty,
+  sum(b.inventory_val) as inv_value,
+  sum(b.si_sls_qty) as sell_in_qty,
+  sum(b.si_gts_val) as sell_in_value
+from all_months, wks_thailand_base as b
+where
+  all_months.sap_parent_customer_key = b.sap_prnt_cust_key(+)
+  and all_months.sap_parent_customer_desc = b.sap_prnt_cust_desc(+)
+  and all_months.matl_num = b.sku_cd(+)
+  and all_months.mnth_id = b.cal_mnth_id(+)
+  and upper(all_months.dstr_nm) = upper(b.dstr_nm(+))
+group by
+  all_months.dstr_nm,
+  all_months.sap_parent_customer_key,
+  all_months.sap_parent_customer_desc,
+  all_months.matl_num,
+  all_months.mnth_id
+ )
+select * from transformed
---
+++ models/thailand/thawks_integration/inventory_health/intermediate/thawks_integration__wks_thailand_base_detail.sql
@@ -0,0 +1,86 @@
+with edw_vw_os_time_dim as(
+select * from {{ ref('sgpedw_integration__edw_vw_os_time_dim') }}
+),
+wks_thailand_lastnmonths as (
+select * from {{ ref('thawks_integration__wks_thailand_lastnmonths') }}
+),
+wks_thailand_base as (
+select * from dev_dna_core.snaposewks_integration.wks_thailand_base
+),
+trans as(
+  select
+    agg.dstr_nm,
+    agg.sap_parent_customer_key,
+    agg.sap_parent_customer_desc,
+    agg.matl_num,
+    agg.month,
+    base.sku_cd as base_matl_num,
+    base.so_sls_qty as so_qty,
+    base.so_grs_trd_sls as so_val,
+    base.inventory_quantity as closing_stock,
+    base.inventory_val as closing_stock_val,
+    base.si_sls_qty as sell_in_qty,
+    base.si_gts_val as sell_in_value,
+    agg.last_3months_so,
+    agg.last_6months_so,
+    agg.last_12months_so,
+    agg.last_3months_so_value,
+    agg.last_6months_so_value,
+    agg.last_12months_so_value,
+    agg.last_36months_so_value,
+    case when (
+      base.sku_cd is null
+    ) then 'Y' else 'N' end as replicated_flag
+  from wks_thailand_lastnmonths as agg, wks_thailand_base as base
+  where
+    left(agg.month, 4) >= (
+      date_part(year, current_timestamp()) - 2
+    )
+    and agg.sap_parent_customer_key = base.sap_prnt_cust_key(+)
+    and upper(agg.sap_parent_customer_desc) = upper(base.sap_prnt_cust_desc(+))
+    and upper(agg.dstr_nm) = upper(base.dstr_nm(+))
+    and agg.matl_num = base.sku_cd(+)
+    and agg.month = base.cal_mnth_id(+)
+    and agg.month <= (
+      select distinct
+        cal_mnth_id as mnth_id
+      from edw_vw_os_time_dim
+      where
+        cal_date = current_date()
+    
+		)
+),
+final as (
+
+select
+  dstr_nm,
+  sap_parent_customer_key,
+  sap_parent_customer_desc,
+  matl_num,
+  month,
+  matl_num as base_matl_num,
+  replicated_flag,
+  sum(so_qty) as so_qty,
+  sum(so_val) as so_value,
+  sum(closing_stock) as inv_qty,
+  sum(closing_stock_val) as inv_value,
+  sum(sell_in_qty) as sell_in_qty,
+  sum(sell_in_value) as sell_in_value,
+  sum(last_3months_so) as last_3months_so,
+  sum(last_6months_so) as last_6months_so,
+  sum(last_12months_so) as last_12months_so,
+  sum(last_3months_so_value) as last_3months_so_value,
+  sum(last_6months_so_value) as last_6months_so_value,
+  sum(last_12months_so_value) as last_12months_so_value,
+  sum(last_36months_so_value) as last_36months_so_value
+from trans
+group by
+  dstr_nm,
+  sap_parent_customer_key,
+  sap_parent_customer_desc,
+  matl_num,
+  month,
+  replicated_flag
+)
+
+select * from final
