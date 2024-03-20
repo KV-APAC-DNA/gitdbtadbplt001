6ff06268c2796824859e9a9c4909d23d416879f9
Akash Pahilwan <apahil01@kenvue.com>
2024-03-20 06:53:19 +0000

    Added models for file_ingestion

---
+++ models/thailand/thaitg_integration/gt/raw/thaitg_integration__sdl_raw_la_gt_sales_order_fact.sql
@@ -0,0 +1,80 @@
+{{
+    config(
+        materialized="incremental",
+        incremental_strategy="append"
+    )
+}}
+
+with source as (
+    select * from {{ source('thasdl_raw', 'sdl_la_gt_sales_order_fact') }}
+),
+final as (
+     select 
+        saleunit,
+        orderid,
+        orderdate,
+        customer_id,
+        customer_name,
+        city,
+        region,
+        saledistrict,
+        saleoffice,
+        salegroup,
+        customertype,
+        storetype,
+        saletype,
+        salesemployee,
+        salename,
+        productid,
+        productname,
+        megabrand,
+        brand,
+        baseproduct,
+        variant,
+        putup,
+        priceref,
+        backlog,
+        qty,
+        subamt1,
+        discount,
+        subamt2,
+        discountbtline,
+        totalbeforevat,
+        total,
+        no,
+        canceled,
+        documentid,
+        return_reason,
+        promotioncode,
+        promotioncode1,
+        promotioncode2,
+        promotioncode3,
+        promotioncode4,
+        promotioncode5,
+        promotion_code,
+        promotion_code2,
+        promotion_code3,
+        avgdiscount,
+        ordertype,
+        approverstatus,
+        pricelevel,
+        optional3,
+        deliverydate,
+        ordertime,shipto,
+        billto,
+        deliveryrouteid,
+        approved_date,
+        approved_time,
+        ref_15,
+        paymenttype,
+        filename,
+        run_id,
+        crt_dttm 
+    from source
+    {% if is_incremental() %}
+        -- this filter will only be applied on an incremental run
+        where crt_dttm > (select max(crt_dttm) from {{ this }}) 
+    {% endif %}
+)
+
+select * from final
