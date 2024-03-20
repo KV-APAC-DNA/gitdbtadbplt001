2f9522d0300d57d5eabc5d8d571b6970666e6955
Niketh Narasimhan <nnaras01@kenvue.com>
2024-03-20 15:05:59 +0000

    all pending models validated

---
+++ macros/thailand/gt/build_wks_la_gt_route_detail_hashkey.sql
@@ -0,0 +1,67 @@
+{% macro build_wks_la_gt_route_detail_hashkey() %}
+    
+    {% set query %}
+        CREATE TABLE if not exists thawks_integration.wks_la_gt_route_detail_hashkey (
+            	hashkey varchar(500),
+                route_id varchar(50),
+                customer_id varchar(50),
+                route_no varchar(10),
+                saleunit varchar(100),
+                ship_to varchar(50),
+                contact_person varchar(100),
+                created_date date,
+                file_upload_date date,
+                last_modified_date date,
+                flag varchar(5),
+                filename varchar(50),
+                run_id varchar(14),
+                crt_dttm timestamp_ntz(9)
+        );
+        TRUNCATE TABLE thawks_integration.wks_la_gt_route_detail_hashkey;
+        INSERT INTO thawks_integration.wks_la_gt_route_detail_hashkey
+            (
+            hashkey,
+            route_id,
+            customer_id,
+            route_no,
+            saleunit,
+            ship_to,
+            contact_person,
+            created_date,
+            file_upload_date,
+            last_modified_date,
+            flag,
+            filename,
+            run_id,
+            crt_dttm
+            )
+        SELECT MD5(COALESCE(UPPER(TRIM(saleunit)),'N/A') ||COALESCE (UPPER(TRIM(customer_id)),'N/A') ||COALESCE (UPPER(TRIM(route_id)),'N/A') ||COALESCE (UPPER(TRIM(route_no)),'N/A')) AS hashkey,
+            route_id,
+            customer_id,
+            route_no,
+            saleunit,
+            ship_to,
+            contact_person,
+            created_date,
+            last_modified_date,
+            file_upload_date,
+            flag,
+            filename,
+            run_id,
+            crt_dttm
+FROM 
+{% if target=='prod' %}
+        thaitg_integration.itg_la_gt_route_detail
+    {% else %}
+        {{schema}}.thaitg_integration__itg_la_gt_route_detail
+    {% endif %}
+WHERE UPPER(TRIM(saleunit)) IN (SELECT DISTINCT UPPER(TRIM(saleunit))
+                                FROM dev_dna_load.snaposesdl_raw.sdl_la_gt_route_detail)
+AND   UPPER(flag) IN ('I','U');
+
+                                --{{ source('thasdl_raw', 'sdl_la_gt_route_detail') }}
+    {% endset %}
+
+    {% do run_query(query) %}
+{% endmacro %}
+
---
+++ models/thailand/thaitg_integration/gt/history/thaitg_integration__itg_la_gt_route_detail.sql
@@ -0,0 +1,32 @@
+--pre_hook="delete from {{this}} where (coalesce(upper(trim(saleunit)),'N/A')) in (select distinct coalesce(upper(trim(saleunit)),'N/A')
+   --                                               from {{ source('thasdl_raw', 'sdl_la_gt_route_detail') }}) and   upper(flag) in ('I','U');"
+
+{{
+    config(
+        materialized="incremental",
+        incremental_strategy='append',
+        pre_hook="delete from {{this}} where (coalesce(upper(trim(saleunit)),'N/A')) in (select distinct coalesce(upper(trim(saleunit)),'N/A')
+                                                  from dev_dna_load.snaposesdl_raw.sdl_la_gt_route_detail) and   upper(flag) in ('I','U');"
+    )
+}}
+with wks_la_gt_route_detail_pre_load as (
+    select * from {{ ref('thawks_integration__wks_la_gt_route_detail_pre_load') }}
+),
+final as (
+SELECT 
+route_id::varchar(50) as route_id,
+customer_id::varchar(50) as customer_id,
+route_no::varchar(10) as route_no,
+saleunit::varchar(100) as saleunit,
+ship_to::varchar(50) as ship_to,
+contact_person::varchar(100) as contact_person,
+created_date::date as created_date,
+file_upload_date::date as file_upload_date,
+last_modified_date::date as last_modified_date,
+flag::varchar(5) as flag,
+filename::varchar(50) as filename,
+run_id::varchar(14) as run_id,
+crt_dttm::timestamp_ntz(9) as crt_dttm,
+current_timestamp()::timestamp_ntz(9) as updt_dttm,
+FROM wks_la_gt_route_detail_pre_load )
+select * from final
