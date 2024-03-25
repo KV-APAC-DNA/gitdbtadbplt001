d6c659d7fe99bebfe6db1f1c95c17c629ff38b96
Niketh Narasimhan <nnaras01@kenvue.com>
2024-03-25 08:54:50 +0000

    os_wks.wks_mds_vn_allchannel_siso_target_sku ,os_itg.itg_mds_vn_allchannel_siso_target_sku os_itg.itg_mds_vn_ecom_target os_wks.wks_mds_vn_ecom_target

---
+++ models/vietnam/vnmitg_integration/vnmitg_integration__itg_mds_vn_allchannel_siso_target_sku.sql
@@ -0,0 +1,30 @@
+{{
+    config
+    (
+        materialized="incremental",
+        incremental_strategy="delete+insert",
+        unique_key=["cycle","data_type","jnj_code"]
+    )
+}}
+with wks_mds_vn_allchannel_siso_target_sku as (
+    select * from DEV_DNA_CORE.SNAPOSEWKS_INTEGRATION.WKS_MDS_VN_ALLCHANNEL_SISO_TARGET_SKU
+
+)
+final as (
+select
+    cycle::number(10,0) as cycle,
+    data_type::varchar(30) as data_type,
+    jnj_code::number(31,0) as jnj_code,
+    description::varchar(200) as description,
+    dksh_mti::number(31,7) as dksh_mti,
+    dksh_ecom::number(31,7) as dksh_ecom,
+    mtd::number(31,7) as mtd,
+    gt_tien_thanh::number(31,7) as gt_tien_thanh,
+    gt_duong_anh::number(31,7) as gt_duong_anh,
+    run_id::number(14,0) as run_id,
+    crtd_dttm::timestamp_ntz(9) as crtd_dttm,
+    updt_dttm::timestamp_ntz(9) as updt_dttm,
+    otc::varchar(100) as otc
+from wks_mds_vn_allchannel_siso_target_sku
+)
+select * from final 
---
+++ models/vietnam/vnmwks_integration/vnmwks_integration__wks_mds_vn_allchannel_siso_target_sku.sql
@@ -0,0 +1,21 @@
+with sdl_mds_vn_allchannel_siso_target_sku as (
+    select * from DEV_DNA_LOAD.SNAPOSESDL_RAW.SDL_MDS_VN_ALLCHANNEL_SISO_TARGET_SKU
+
+),
+final as (
+select 
+    cycle::number(31,0) as cycle,
+    "data type"::varchar(200) as data_type,
+    "jnj code"::number(31,0) as jnj_code,
+    description::varchar(200) as description,
+    "dksh - mti"::number(31,7) as dksh_mti,
+    "dksh - ecom"::number(31,7) as dksh_ecom,
+    mtd::number(31,7) as mtd,
+    "gt - tien thanh"::number(31,7) as gt_tien_thanh,
+    "gt - duong anh"::number(31,7) as gt_duong_anh,
+    '0'::varchar(1) as run_id,
+    current_timestamp()::timestamp_ntz(9) as crtd_dttm,
+    current_timestamp()::timestamp_ntz(9) as updt_dttm,
+    otc::varchar(100) as otc,
+from sdl_mds_vn_allchannel_siso_target_sku )
+select * from final
