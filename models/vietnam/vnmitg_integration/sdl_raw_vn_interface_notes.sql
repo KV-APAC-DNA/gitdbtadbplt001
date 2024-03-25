8991f53bcb136d53c56f793fed08776bd2b23c48
Niketh Narasimhan <nnaras01@kenvue.com>
2024-03-25 07:23:39 +0000

    os_sdl.sdl_raw_vn_interface_choices,os_itg.itg_vn_interface_choices ,os_sdl.sdl_raw_vn_interface_notes ,os_itg.itg_vn_interface_notes

---
+++ models/vietnam/vnmitg_integration/sdl_raw_vn_interface_notes.sql
@@ -0,0 +1,31 @@
+
+{{
+    config(
+        materialized="incremental",
+        incremental_strategy="append"
+        )
+}}
+
+with sdl_vn_interface_notes as (
+    select * from DEV_DNA_LOAD.SNAPOSESDL_RAW.sdl_vn_interface_notes
+),
+final as (
+select 
+cust_code::varchar(255) as cust_code,
+slsper_id::varchar(255) as slsper_id,
+shop_code::varchar(255) as shop_code,
+ise_id::varchar(255) as ise_id,
+ques_no::number(10,0) as ques_no,
+answer_seq::number(20,0) as answer_seq,
+answer_value::varchar(255) as answer_value,
+createddate::varchar(255) as createddate,
+filename::varchar(255) as filename,
+crt_dttm::timestamp_ntz(9) as crt_dttm
+from  sdl_vn_interface_notes
+{% if is_incremental() %}
+    -- this filter will only be applied on an incremental run
+    where source.crt_dttm > (select max(crt_dttm) from {{ this }}) 
+ {% endif %}
+)
+select * from final
+
