ba5c1c4bf606e2ff59a1a583f80586ede2e9ac3a
Ashika Singh <ASing012@kenvue.com>
2024-03-27 17:35:36 +0000

    Models created

---
+++ models/vietnam/vnmedw_integration/pos/view/vnmedw_integration__edw_vw_vn_mt_pos_price_products.sql
@@ -0,0 +1,20 @@
+with itg_vn_mt_pos_price_products as 
+(
+    select * from snaposeitg_integration.itg_vn_mt_pos_price_products
+),
+final as 
+(
+select itg_vn_mt_pos_price_products.jnj_sap_code,
+    itg_vn_mt_pos_price_products.franchise,
+    itg_vn_mt_pos_price_products.brand,
+    itg_vn_mt_pos_price_products.sku,
+    itg_vn_mt_pos_price_products.bar_code,
+    itg_vn_mt_pos_price_products.pc_per_case,
+    itg_vn_mt_pos_price_products.price,
+    itg_vn_mt_pos_price_products.product_id_concung
+from itg_vn_mt_pos_price_products
+where (
+        (itg_vn_mt_pos_price_products.active)::text = ('Y'::character varying)::text
+    )
+)
+select * from final
---
+++ models/vietnam/vnmitg_integration/distributor/master/vnmitg_integration__itg_vn_dms_distributor_dim.sql
@@ -0,0 +1,34 @@
+{{
+    config(
+        materialized="incremental",
+        incremental_strategy= "delete+insert",
+        unique_key=  ['dstrbtr_id']
+    )
+}}
+
+with source as (
+    select * from {{ source('vnmsdl_raw','sdl_vn_dms_distributor_dim') }}
+),
+
+final as
+(
+select
+    territory_dist::varchar(100) as territory_dist,
+    mapped_spk::varchar(30) as mapped_spk,
+    dstrbtr_id::varchar(30) as dstrbtr_id,
+    dstrbtr_type::varchar(100) as dstrbtr_type,
+    dstrbtr_name::varchar(100) as dstrbtr_name,
+    dstrbtr_address::varchar(200) as dstrbtr_address,
+    trim(longitude, ',')::varchar(20) as longitude,
+    trim(latitude, ',')::varchar(20) as latitude,
+    region::varchar(20) as region,
+    province::varchar(100) as province,
+    trim(active, ',')::varchar(5) as active,
+    trim(asm_id)::varchar(50) as asm_id,
+	current_timestamp()::timestamp_ntz(9) as crt_dttm,
+	current_timestamp()::timestamp_ntz(9) as updt_dttm_dttm,
+    null::number(14,0) as run_id    
+from source
+)
+select * from final
+   
