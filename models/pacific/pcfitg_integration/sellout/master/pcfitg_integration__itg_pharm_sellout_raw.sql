d539ff62ed6bf856391f9ff32fc5e40fb5049a0a
Ashika Singh <ASing012@kenvue.com>
2024-04-11 13:22:13 +0000

    Models created

---
+++ models/pacific/pcfitg_integration/sellout/master/pcfitg_integration__itg_pharm_sellout_raw.sql
@@ -0,0 +1,29 @@
+
+{{
+    config(
+        materialized="incremental",
+        incremental_strategy= "delete+insert",
+        unique_key=  ['time_period']
+    )
+}}
+
+with source as (
+    select * from {{ source('pcfsdl_raw','sdl_pharm_sellout_weekly_raw') }}
+),
+final as
+(
+select 
+    outlet_number::varchar(10) as store_probe_id,
+	name::varchar(30) as store_name,
+	pfc::varchar(10) as product_probe_id,
+	pack_long_desc::varchar(60) as product_description,
+	week_ending_date::varchar(8) as time_period,
+	units::number(18,0) as units,
+	amount::number(18,6) as src_amt_value,
+	amount::number(18,6) as amount, --check for this column as repeated above
+	derived_price::number(12,6) as derived_price,
+	current_timestamp()::timestamp_ntz(9) as crt_dttm,
+	current_timestamp()::timestamp_ntz(9) as updt_dttm
+from source
+)
+select * from final
