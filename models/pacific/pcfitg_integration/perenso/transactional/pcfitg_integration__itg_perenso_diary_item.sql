5e0be1ec8e27ed780a49d67649137a2520d03269
Anjali Agrawal <AAgraw03@kenvue.com>
2024-04-23 12:53:05 +0000

    fixed the issue

--- models/pacific/pcfitg_integration/perenso/transactional/pcfitg_integration__itg_perenso_diary_item.sql
+++ models/pacific/pcfitg_integration/perenso/transactional/pcfitg_integration__itg_perenso_diary_item.sql
@@ -8,30 +8,20 @@
     )
 }}
 
-
 with sdl_perenso_diary_item as (
-select * from {{ source('pcfsdl_raw', 'sdl_perenso_diary_item') }}
+    select * from {{ source('pcfsdl_raw', 'sdl_perenso_diary_item') }}
 ),
 final as (
-SELECT diary_item_key::number(10,0),
-
-       diary_item_type_key::number(10,0),
-
-       TO_TIMESTAMP(SUBSTRING(START_TIME,0,23),'DD/MM/YYYY HH12:MI:SS AM') as start_time,
-       TO_TIMESTAMP(SUBSTRING(end_time,0,23),'DD/MM/YYYY HH12:MI:SS AM') as start_time,
-
-       acct_key::number(10,0),
-
-       acct_key_1::number(10,0),
-
-       create_user_key::number(10,0),
-
-       complete::varchar(5),
-
-       run_id::number(14,0),
-
-       current_timestamp()::timestamp_ntz(9) as create_dt
-
-FROM sdl_perenso_diary_item
+    SELECT diary_item_key::number(10,0),
+        diary_item_type_key::number(10,0),
+        TO_TIMESTAMP(SUBSTRING(START_TIME,0,23),'DD/MM/YYYY HH12:MI:SS AM') as start_time,
+        TO_TIMESTAMP(SUBSTRING(end_time,0,23),'DD/MM/YYYY HH12:MI:SS AM') as end_time,
+        acct_key::number(10,0),
+        acct_key_1::number(10,0),
+        create_user_key::number(10,0),
+        complete::varchar(5),
+        run_id::number(14,0),
+        current_timestamp()::timestamp_ntz(9) as create_dt
+    FROM sdl_perenso_diary_item
 )
 select * from final
