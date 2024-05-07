cc5cf476828e79404bb2eaec07181b9632166b8b
Swetha Nenavath <snenav01@kenvue.com>
2024-05-07 06:23:56 +0000

    added inventory models

---
+++ models/philippines/phlitg_integration/pos/master/phlitg_integration__itg_ph_pricelist.sql
@@ -0,0 +1,34 @@
+{{
+    config
+    (
+        materialized="incremental",
+        incremental_strategy= "delete+insert",
+        unique_key= ["jj_mnth_id","item_cd"]
+    )
+}}
+
+with sdl_ph_pricelist as (
+    select * from {{ source('phlsdl_raw', 'sdl_ph_pricelist') }}
+),
+final as (
+select 
+jj_mnth_id::varchar(10) as jj_mnth_id,
+item_cd::varchar(20) as item_cd,
+item_desc::varchar(80) as item_desc,
+consumer_bar_cd::varchar(20) as consumer_bar_cd,
+shippers_bar_cd::varchar(20) as shippers_bar_cd,
+dz_per_case::number(15,4) as dz_per_case,
+lst_price_case::number(15,4) as lst_price_case,
+lst_price_dz::number(15,4) as lst_price_dz,
+lst_price_unit::number(15,4) as lst_price_unit,
+srp::number(15,4) as srp,
+status::varchar(15) as status,
+sap_item_desc::varchar(50) as sap_item_desc,
+brnd_vrnt::varchar(100) as brnd_vrnt,
+frnchse_cd::varchar(50) as frnchse_cd,
+cdl_dttm::varchar(20) as cdl_dttm,
+current_timestamp()::timestamp_ntz(9) as crtd_dttm,
+null::timestamp_ntz(9) as updt_dttm
+from sdl_ph_pricelist
+)
+select * from final
---
+++ models/philippines/phlitg_integration/product/master/phlitg_integration__itg_ph_dyna_product_dim.sql
@@ -0,0 +1,20 @@
+with source as(
+	select * from {{ source('phlsdl_raw', 'sdl_ph_dyna_item_ref_dim') }}
+),
+final as(
+	select 
+		yearmo::varchar(20) as yearmonth,
+		'DYNA'::varchar(20) as cust_cd,
+		cust_item_cd::varchar(30) as item_cd,
+		cust_item_desc::varchar(150) as item_nm,
+		jj_item_cd::varchar(30) as sap_item_cd,
+		cust_item_grp::varchar(150) as cust_sku_grp,
+		conv_factor::varchar(20) as cust_conv_factor,
+		null::number(10,2) as cust_item_prc,
+		last_prd::varchar(10) as lst_period,
+		early_bk_prd::varchar(10) as early_bk_period,
+		current_timestamp()::timestamp_ntz(9) as crtd_dttm,
+		current_timestamp()::timestamp_ntz(9) as updt_dttm
+	from source
+)
+select * from final
