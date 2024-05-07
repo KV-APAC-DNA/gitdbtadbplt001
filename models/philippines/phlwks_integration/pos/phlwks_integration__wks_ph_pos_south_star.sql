bea67b8b04ba9b99697ca3498a77ed463c9f3956
Ashika Singh <ASing012@kenvue.com>
2024-05-07 10:28:21 +0000

    Modified Code

--- models/philippines/phlwks_integration/pos/phlwks_integration__wks_ph_pos_south_star.sql
+++ models/philippines/phlwks_integration/pos/phlwks_integration__wks_ph_pos_south_star.sql
@@ -1,6 +1,6 @@
 with sdl_ph_pos_south_star as (
     select * from {{ source('phlsdl_raw', 'sdl_ph_pos_south_star') }}
-)
+),
 final as (
     select * from sdl_ph_pos_south_star
 )
--- models/philippines/phlwks_integration/pos/phlwks_integration__wks_ph_pos_watsons.sql
+++ models/philippines/phlwks_integration/pos/phlwks_integration__wks_ph_pos_watsons.sql
@@ -1,6 +1,6 @@
 with sdl_ph_pos_watsons as (
     select * from {{ source('phlsdl_raw', 'sdl_ph_pos_watsons') }}
-)
+),
 final as (
     select * from sdl_ph_pos_watsons
 )
