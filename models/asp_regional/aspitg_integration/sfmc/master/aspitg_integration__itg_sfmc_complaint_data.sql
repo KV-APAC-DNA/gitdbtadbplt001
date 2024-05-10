7a34e3f9efa4fe9473b6b7613334bf6d9467a260
Niketh Narasimhan <nnaras01@kenvue.com>
2024-05-10 06:26:25 +0000

    rough commit taiwan

--- models/asp_regional/aspitg_integration/sfmc/master/aspitg_integration__itg_sfmc_complaint_data.sql
+++ models/asp_regional/aspitg_integration/sfmc/master/aspitg_integration__itg_sfmc_complaint_data.sql
@@ -8,6 +8,9 @@
                     {% elif var('sfmc_job_to_execute') == 'ph_sfmc_files' %}
                     delete from {{this}} where event_date >= (select min(event_date) from {{ source('phlsdl_raw','sdl_ph_sfmc_complaint_data') }}) and cntry_cd = 'PH';
                     {% endif %}
+                    {% elif var('sfmc_job_to_execute') == 'tw_sfmc_files' %}
+                    delete from {{this}} where event_date >= (select min(event_date) from {{ source('ntasdl_raw','sdl_tw_sfmc_complaint_data') }}) and cntry_cd = 'TW';
+                    {% endif %}
                 "
     )
 }}
@@ -18,6 +21,9 @@ with source as(
 source_ph as
 (
     select *,dense_rank() over(partition by null order by file_name desc) as rnk from {{ source('phlsdl_raw', 'sdl_ph_sfmc_complaint_data') }}
+),
+source_tw as (
+    select *,dense_rank() over(partition by null order by file_name desc) as rnk from {{ source('ntasdl_raw', 'sdl_tw_sfmc_complaint_data') }}
 )
 {% if var("sfmc_job_to_execute") == 'th_sfmc_files' %}
 ,
@@ -68,5 +74,27 @@ final as(
     where rnk=1
 )
 select * from final
-
+{% elif var("sfmc_job_to_execute") == 'tw_sfmc_files' %}
+,
+final as(
+    select
+        'TW'::varchar(10) as cntry_cd,
+        oyb_account_id::varchar(50) as oyb_account_id,
+        job_id::varchar(50) as job_id,
+        list_id::varchar(50) as list_id,
+        batch_id::varchar(50) as batch_id,
+        subscriber_id::varchar(50) as subscriber_id,
+        subscriber_key::varchar(100) as subscriber_key,
+        event_date::timestamp_ntz(9) as event_date,
+        is_unique::varchar(10) as is_unique,
+        domain::varchar(50) as domain,
+        trim(email_subject)::varchar(200) as email_subject,
+        email_name::varchar(100) as email_name,
+        email_id::varchar(100) as email_id,
+        file_name::varchar(255) as file_name,
+        current_timestamp()::timestamp_ntz(9) as crtd_dttm,
+        current_timestamp()::timestamp_ntz(9) as updt_dttm
+    from source_ph
+    where rnk=1
+)
 {% endif %}
---
+++ models/north_asia/ntaitg_integration/rg_watsons/itg/ntaitg_integration__itg_tw_as_watsons_inventory.sql
@@ -0,0 +1,49 @@
+
+
+
+{{
+    config(
+        materialized="incremental",
+        incremental_strategy= "append",
+        unique_key=  ['filename'],
+        pre_hook= "delete from {{this}} where nvl(year,'#')||nvl(week_no,'#')||nvl(item_cd,'#') in (select distinct nvl(year,'#')||nvl(week_no,'#')||nvl(item_cd,'#') from  {{ source('ntasdl_raw', 'sdl_tw_as_watsons_inventory') }});"
+    )
+}}
+
+
+with
+sdl_tw_as_watsons_inventory as (
+    select * from {{ source('ntasdl_raw', 'sdl_tw_as_watsons_inventory') }}
+),
+final as (
+select
+year::varchar(30) as year,
+week_no::varchar(30) as week_no,
+supplier::varchar(30) as supplier,
+item_cd::varchar(30) as item_cd,
+buy_code::varchar(30) as buy_code,
+home_cdesc::varchar(255) as home_cdesc,
+prdt_grp::varchar(30) as prdt_grp,
+grp_desc::varchar(255) as grp_desc,
+prdt_cat::varchar(30) as prdt_cat,
+category_desc::varchar(255) as category_desc,
+item_desc::varchar(255) as item_desc,
+type::varchar(50) as type,
+avg_sls_cost_value::number(20,4) as avg_sls_cost_value,
+total_stock_qty::number(20,4) as total_stock_qty,
+total_stock_value::number(20,4) as total_stock_value,
+weeks_holding_sales::number(20,4) as weeks_holding_sales,
+weeks_holding::number(20,4) as weeks_holding,
+first_recv_date::date as first_recv_date,
+turn_type_sales::varchar(100) as turn_type_sales,
+turn_type::varchar(100) as turn_type,
+uda73::varchar(50) as uda73,
+discontinue_date::date as discontinue_date,
+stock_class::varchar(30) as stock_class,
+pog::varchar(30) as pog,
+ean_num::varchar(30) as ean_num,
+filename::varchar(255) as filename,
+crtd_dttm::timestamp_ntz(9) as crtd_dttm
+from sdl_tw_as_watsons_inventory
+)
+select * from sdl_tw_as_watsons_inventory
