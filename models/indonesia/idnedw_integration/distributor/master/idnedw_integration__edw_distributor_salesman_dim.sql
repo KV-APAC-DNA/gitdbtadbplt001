da650831287c589e06552faaf9315b17aafec8d2
Ashika Singh <ASing012@kenvue.com>
2024-05-21 12:02:13 +0000

    Models created

---
+++ models/indonesia/idnedw_integration/distributor/master/idnedw_integration__edw_distributor_salesman_dim.sql
@@ -0,0 +1,28 @@
+{{
+    config
+    (
+        materialized="incremental",
+        incremental_strategy= "append",
+    )
+}}
+with source as 
+(
+    select * from snapidnedw_integration.edw_distributor_ivy_user_master
+),
+final as
+(
+    select 
+    distinct 
+       dis_code as jj_sap_dstrbtr_id,
+       dis_name as jj_sap_dstrbtr_nm,
+       sr_code as slsmn_id,
+       sr_name as slsmn_nm,
+       (dis_code||sr_code) as rec_key,
+       current_timestamp()::timestamp_ntz(9) as crtd_dttm,
+       current_timestamp()::timestamp_ntz(9) as updt_dttm,
+       sr_code as sfa_id,
+	   '200001' as effective_from,
+	   '999912' as effective_to 
+    from source
+)
+select * from final
