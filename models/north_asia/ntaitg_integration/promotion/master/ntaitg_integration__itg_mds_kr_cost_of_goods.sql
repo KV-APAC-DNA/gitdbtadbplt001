2d30cf25e41fc5a3f881eab59aa541c49ece2bc8
Anjali Agrawal <AAgraw03@kenvue.com>
2024-05-22 13:46:37 +0000

    updated the code

--- models/north_asia/ntaitg_integration/promotion/master/ntaitg_integration__itg_mds_kr_cost_of_goods.sql
+++ models/north_asia/ntaitg_integration/promotion/master/ntaitg_integration__itg_mds_kr_cost_of_goods.sql
@@ -5,11 +5,11 @@ final as (
     select
         sap_code::varchar(200) as sap_code,
         sku_name::varchar(510) as sku_name,
-        free_good_value::number(31,3) as free_good_value,
-        pre_apsc_cogs::number(31,3) as pre_apsc_cogs,
-        package_cost::number(31,3) as package_cost,
-        labour_cost::number(31,3) as labour_cost,
-        attribute_value::number(31,3) as attribute_value,
+        trunc(free_good_value,3)::number(31,3) as free_good_value,
+        trunc(pre_apsc_cogs,3)::number(31,3) as pre_apsc_cogs,
+        trunc(package_cost,3)::number(31,3) as package_cost,
+        trunc(labour_cost,3)::number(31,3) as labour_cost,
+        trunc(attribute_value,3)::number(31,3) as attribute_value,
         valid_from::date as valid_from,
         valid_to::date as valid_to,
         current_timestamp()::timestamp_ntz(9) as crt_dttm
