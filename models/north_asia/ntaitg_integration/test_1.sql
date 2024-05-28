1f72e918e0e1e9913c0ab7bc24f9273b8bb097de
Amit Joshi <AJoshi03@kenvue.com>
2024-05-28 05:49:24 +0000

    commit

--- models/north_asia/ntaitg_integration/test_1.sql
+++ models/north_asia/ntaitg_integration/test_1.sql
@@ -1,6 +1,6 @@
 with source as
 (
-    select * from {{ source('ntasdl_raw', 'sdl_kr_bo_young_jong_hap_logistics_gt_sellout') }}
+    select * from {{ source('ntasdl_raw', 'sdl_kr_lotte_ak_gt_sellout') }}
 ),
 final as
 (
