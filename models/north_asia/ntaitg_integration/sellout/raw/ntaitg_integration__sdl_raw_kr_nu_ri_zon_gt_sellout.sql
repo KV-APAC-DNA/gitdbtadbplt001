2753bad02c3bdc26a11fa793326e0db17bc93a44
Niketh Narasimhan <nnaras01@kenvue.com>
2024-05-30 16:23:21 +0000

    ok

---
+++ models/north_asia/ntaitg_integration/sellout/raw/ntaitg_integration__sdl_raw_kr_nu_ri_zon_gt_sellout.sql
@@ -0,0 +1,29 @@
+{{
+    config(
+        materialized='incremental',
+        incremental_strategy= "append"
+    )
+}}
+
+with sdl_kr_nu_ri_zon_gt_sellout as (
+    select * from {{ source('ntasdl_raw', 'sdl_kr_nu_ri_zon_gt_sellout') }}
+),
+final as (
+SELECT dstr_nm,
+  ims_txn_dt,
+  sub_customer_name,
+  ean,
+  name,
+  standard,
+  qty,
+  unit_price,
+  total,
+  cust_cd,
+  current_timestamp() as crtd_dttm
+FROM sdl_kr_nu_ri_zon_gt_sellout
+{% if is_incremental() %}
+    -- this filter will only be applied on an incremental run
+    where source.crtd_dttm > (select max(crtd_dttm) from {{ this }}) 
+    {% endif %}
+)
+select * from final 
