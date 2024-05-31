aadd480c6dbe2e49409a9834c41b86e2debeeca8
Vaibhav Shrirao <VShrir01@kenvue.com>
2024-05-31 08:14:22 +0000

    models created

---
+++ models/indonesia/idnedw_integration/perfect_store/master/idnedw_integration__edw_id_ps_outlet_master.sql
@@ -0,0 +1,61 @@
+{{ config(materialized='table')}}
+
+with itg_id_ps_product_availability as 
+(
+    select * from {{ref('idnitg_integration__itg_id_ps_product_availability')}}
+),
+itg_id_ps_msl_reference as
+(
+    select * from {{ref('idnitg_integration__itg_id_ps_msl_reference')}}
+),
+pa as 
+(
+    (select distinct 
+        pa.outlet_id,
+        coalesce(pa.outlet_custom_code,'NA') outlet_custom_code,
+        pa.outlet_name,
+        pa.province,
+        pa.city,
+        pa.channel,
+        msl.channel_group,
+        pa.cust_group,
+        pa.address,
+        convert_timezone('UTC', current_timestamp())::timestamp_ntz(9) as crt_dttm,
+        row_number() over (partition by pa.outlet_id order by input_date desc) rn
+    from itg_id_ps_product_availability pa
+    left join itg_id_ps_msl_reference msl
+            on upper (pa.channel) = upper (msl.channel)
+            and upper (pa.cust_group) = upper (msl.cust_group))
+),
+transformed as  
+(
+    select 
+        outlet_id,
+        outlet_custom_code,
+        outlet_name,
+        province,
+        city,
+        channel,
+        channel_group,
+        cust_group,
+        address,
+        crt_dttm
+    from pa
+    where rn = 1
+),
+final as 
+(
+    select
+        outlet_id::varchar(10) as outlet_id,
+        outlet_custom_code::varchar(10) as outlet_custom_code,
+        outlet_name::varchar(100) as outlet_name,
+        province::varchar(50) as province,
+        city::varchar(50) as city,
+        channel::varchar(50) as channel,
+        channel_group::varchar(50) as channel_group,
+        cust_group::varchar(50) as cust_group,
+        address::varchar(255) as address,
+        convert_timezone('UTC', current_timestamp())::timestamp_ntz(9) as crt_dttm
+    from transformed
+)
+select * from final
---
+++ models/indonesia/idnitg_integration/ivy/master/idnitg_integration__itg_distributor_ivy_outlet_master.sql
@@ -0,0 +1,179 @@
+{{
+    config
+    (
+        materialized = "incremental",
+        incremental_strategy = "append",
+        pre_hook = "{% if is_incremental() %}
+        delete from {{this}}
+        where (upper(trim(distributorcode)), upper(trim(outletcode))) 
+        in (select distinct upper(trim(distributorcode)) as distributorcode, upper(trim(outletcode)) as outletcode
+        from {{source ('idnsdl_raw', 'sdl_distributor_ivy_outlet_master')}});
+        {% endif %}"
+    )
+}}
+
+with sdl_distributor_ivy_outlet_master as 
+(
+    select * from {{source ('idnsdl_raw', 'sdl_distributor_ivy_outlet_master')}}
+),
+edw_distributor_customer_dim as 
+(
+    select * from {{ref('idnedw_integration__edw_distributor_customer_dim')}}
+),
+edw_channelgroup_metadata as 
+(
+    select * from {{ref('idnedw_integration__edw_channelgroup_metadata')}}
+),
+itg_distributor_ivy_outlet_master as (
+    select 
+        trim(distributorcode) as distributorcode,
+        usercode,
+        locationcode,
+        trim(outletcode) as outletcode,
+        outletname,
+        outletaddress,
+        pincode,
+        subchannelname,
+        classcode,
+        routecode,
+        visit_frequency,
+        visitday,
+        jnj_id,
+        contactperson,
+        credit_limit,
+        invoice_limit,
+        credit_period,
+        tin,
+        is_diamond_store,
+        id_no,
+        master_code,
+        store_cluster,
+        lattitude,
+        longitude,
+        cdl_dttm,
+        channelname,
+        tieringname,
+        run_id,
+        row_number() over (partition by distributorcode, outletcode order by run_id) as rn
+    from SDL_DISTRIBUTOR_IVY_OUTLET_MASTER
+),
+final as (
+    select 
+        distributorcode::varchar(25) as distributorcode,
+        usercode::varchar(25) as usercode,
+        locationcode::varchar(25) as locationcode,
+        outletcode::varchar(25) as outletcode,
+        outletname::varchar(100) as outletname,
+        outletaddress::varchar(255) as outletaddress,
+        pincode::varchar(10) as pincode,
+        subchannelname::varchar(100) as subchannelcode,
+        classcode::varchar(15) as classcode,
+        routecode::varchar(25) as routecode,
+        visit_frequency::varchar(15) as visit_frequency,
+        visitday::varchar(200) as visitday,
+        jnj_id::varchar(20) as jnj_id,
+        contactperson::varchar(255) as contactperson,
+        credit_limit::number(18,2) as credit_limit,
+        invoice_limit::number(18,2) as invoice_limit,
+        credit_period::number(2,0) as credit_period,
+        tin::varchar(50) as tin,
+        is_diamond_store::varchar(3) as is_diamond_store,
+        id_no::varchar(100) as id_no,
+        master_code::varchar(100) as master_code,
+        store_cluster::varchar(50) as store_cluster,
+        lattitude::varchar(20) as lattitude,
+        longitude::varchar(20) as longitude,
+        cdl_dttm::varchar(200) as cdl_dttm,
+        null::varchar(100) as cust_grp,
+        channelname::varchar(100) as chnl,
+        null::varchar(100) as chnl_grp,
+        outletcode::varchar(100) as cust_id_map,
+        outletname::varchar(100) as cust_nm_map, 
+        null::varchar(100) as chnl_grp2,
+        null::timestamp_ntz(9) as cust_crtd_dt,
+        tieringname::varchar(100) as cust_grp2,
+        run_id::number(14,0) as run_id
+    from itg_distributor_ivy_outlet_master
+    where rn = 1
+),
+updt as(
+    select 
+        final.distributorcode as distributorcode,
+        final.usercode as usercode,
+        final.locationcode as locationcode,
+        final.outletcode as outletcode,
+        final.outletname as outletname,
+        final.outletaddress as outletaddress,
+        final.pincode as pincode,
+        final.subchannelcode as subchannelcode,
+        final.classcode as classcode,
+        final.routecode as routecode,
+        final.visit_frequency as visit_frequency,
+        final.visitday as visitday,
+        final.jnj_id as jnj_id,
+        final.contactperson as contactperson,
+        final.credit_limit as credit_limit,
+        final.invoice_limit as invoice_limit,
+        final.credit_period as credit_period,
+        final.tin as tin,
+        final.is_diamond_store as is_diamond_store,
+        final.id_no as id_no,
+        final.master_code as master_code,
+        final.store_cluster as store_cluster,
+        final.lattitude as lattitude,
+        final.longitude as longitude,
+        final.cdl_dttm as cdl_dttm,
+        nvl(EDW_DISTRIBUTOR_CUSTOMER_DIM.CUST_GRP,final.cust_grp) as cust_grp,
+        final.chnl as chnl,
+        nvl(EDW_DISTRIBUTOR_CUSTOMER_DIM.chnl_grp, final.chnl_grp) as chnl_grp,
+        nvl(EDW_DISTRIBUTOR_CUSTOMER_DIM.CUST_GRP,final.cust_id_map) as cust_id_map,
+        nvl(EDW_DISTRIBUTOR_CUSTOMER_DIM.CUST_GRP,final.cust_nm_map) as cust_nm_map,
+        nvl(EDW_DISTRIBUTOR_CUSTOMER_DIM.chnl_grp2, final.chnl_grp2) as chnl_grp2,
+        nvl(EDW_DISTRIBUTOR_CUSTOMER_DIM.cust_crtd_dt, final.cust_crtd_dt) as cust_crtd_dt,
+        final.cust_grp2 as cust_grp2,
+        final.run_id as run_id 
+    from final
+    left join EDW_DISTRIBUTOR_CUSTOMER_DIM
+    on (upper(trim(final.distributorcode)) || upper(trim(final.outletcode))) = upper(trim(EDW_DISTRIBUTOR_CUSTOMER_DIM.KEY_OUTLET))
+),
+updt2 as(
+    select 
+        updt.distributorcode as distributorcode,
+        updt.usercode as usercode,
+        updt.locationcode as locationcode,
+        updt.outletcode as outletcode,
+        updt.outletname as outletname,
+        updt.outletaddress as outletaddress,
+        updt.pincode as pincode,
+        updt.subchannelcode as subchannelcode,
+        updt.classcode as classcode,
+        updt.routecode as routecode,
+        updt.visit_frequency as visit_frequency,
+        updt.visitday as visitday,
+        updt.jnj_id as jnj_id,
+        updt.contactperson as contactperson,
+        updt.credit_limit as credit_limit,
+        updt.invoice_limit as invoice_limit,
+        updt.credit_period as credit_period,
+        updt.tin as tin,
+        updt.is_diamond_store as is_diamond_store,
+        updt.id_no as id_no,
+        updt.master_code as master_code,
+        updt.store_cluster as store_cluster,
+        updt.lattitude as lattitude,
+        updt.longitude as longitude,
+        updt.cdl_dttm as cdl_dttm,
+        updt.cust_grp as cust_grp,
+        updt.chnl as chnl,
+        nvl(channelgroup.chnl_grp, updt.chnl_grp) as chnl_grp,
+        updt.cust_id_map as cust_id_map,
+        updt.cust_nm_map as cust_nm_map,
+        updt.chnl_grp2 as chnl_grp2,
+        updt.cust_crtd_dt as cust_crtd_dt,
+        updt.cust_grp2 as cust_grp2,
+        updt.run_id as run_id
+    from updt 
+    left join EDW_CHANNELGROUP_METADATA channelgroup
+    on updt.cust_grp2 = channelgroup.cust_grp2
+)
+select * from updt2
