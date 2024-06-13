f849215fe4bd6862c397cd81f81b17a188825d8a
SwethaNenavath123 <155425374+SwethaNenavath96@users.noreply.github.com>
2024-06-13 12:22:47 +0530

    added raw models - pop6
    -- added raw models - pop6

---
+++ models/north_asia/ntaitg_integration/pop6/raw/ntaitg_integration__sdl_raw_pop6_kr_exclusion.sql
@@ -0,0 +1,22 @@
+{{
+    config(
+        materialized="incremental",
+        incremental_strategy="append"
+    )}}
+
+with
+source as
+(
+    select * from {{ source('jpnsdl_raw', 'sdl_pop6_kr_exclusion') }}
+),
+
+final as
+(
+    select * from source
+    {% if is_incremental() %}
+    -- this filter will only be applied on an incremental run
+    where crtd_dttm > (select max(crtd_dttm) from {{ this }})
+{% endif %}
+)
+
+select * from final
