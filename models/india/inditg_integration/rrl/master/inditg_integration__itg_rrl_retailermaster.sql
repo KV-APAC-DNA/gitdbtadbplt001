a44fb93154521ceb4e943ef910b5471667d701df
Sandip Kumar Maharaj <SMahar01@kenvue.com>
2024-06-14 10:15:50 +0000

    Validation Completed

--- models/india/inditg_integration/rrl/master/inditg_integration__itg_rrl_retailermaster.sql
+++ models/india/inditg_integration/rrl/master/inditg_integration__itg_rrl_retailermaster.sql
@@ -1,6 +1,15 @@
+{{
+    config(
+        materialized="incremental",
+        incremental_strategy= "append",
+        pre_hook= "{% if is_incremental() %}
+        delete from {{this}} as itg_rrl_retailermaster using {{ ref('indwks_integration__wks_rrl_retailermaster')}} as s where itg_rrl_retailermaster.distributorcode = s.distributorcode and itg_rrl_retailermaster.retailercode=s.retailercode and itg_rrl_retailermaster.rsdcode=s.rsdcode and itg_rrl_retailermaster.villagecode=s.villagecode and itg_rrl_retailermaster.rsrcode=s.rsrcode and s.chng_flg in ('U', 'U2') and itg_rrl_retailermaster.actv_flg='Y';
+        {% endif %}"
+    )
+}}
 with source as
 (
-    select * from {{ source('indsdl_raw', 'sdl_rrl_retailermaster')}}
+    select * from {{ ref('indwks_integration__wks_rrl_retailermaster')}}
 ),
 transformed as
 (
@@ -52,17 +61,12 @@ transformed as
     updt_dttm
     from source
 ),
-final as 
+final as
 (
     select
     retailercode::varchar(50) as retailercode,
-    --CASE 
-    --WHEN chng_flg in ('I', 'U') 
-    --THEN src_retailername
-    --ELSE tgt_retailername
-    --END::varchar(100) RETAILERNAME,
 	retailername::varchar(100) as retailername,
-    routecode::varchar(25) as routecode,
+	routecode::varchar(25) as routecode,
 	retailerclasscode::varchar(50) as retailerclasscode,
 	villagecode::varchar(50) as villagecode,
 	rsdcode::varchar(50) as rsdcode,
@@ -73,35 +77,30 @@ final as
 	phone::varchar(15) as phone,
 	mobile::varchar(15) as mobile,
 	prcontact::varchar(50) as prcontact,
-	seccontact::varchar(50) as seccontact,	
-    creditlimit::number(18,0) as creditlimit,
-    creditperiod::number(18,0) as creditperiod, 
+	seccontact::varchar(50) as seccontact,
+	creditlimit::number(18,0) as creditlimit,
+	creditperiod::number(18,0) as creditperiod,
     invoicelimit::varchar(30) as invoicelimit,
-	isapproved::varchar(1) as isapproved,
-	isactive::boolean as isactive,
+    isapproved::varchar(1) as isapproved,	
+    isactive::boolean as isactive, 
 	rsrcode::varchar(100) as rsrcode,
 	drugvaliditydate::timestamp_ntz(9) as drugvaliditydate,
 	fssaivaliditydate::timestamp_ntz(9) as fssaivaliditydate,
-    --CASE
-    --WHEN chng_flg in ('I', 'U') 
-    --THEN src_displaystatus
-    --ELSE tgt_displaystatus
-    --END::VARCHAR(20) as displaystatus,
 	displaystatus::varchar(20) as displaystatus,
 	createddate::timestamp_ntz(9) as createddate,
 	ownername::varchar(100) as ownername,
 	druglicenseno2::varchar(50) as druglicenseno2,
 	r_statecode::number(18,0) as r_statecode,
-    r_districtcode::number(18,0) as r_districtcode,
-    r_tahsilcode::number(18,0) as r_tahsilcode,  
-    address1::varchar(100) as address1,
+	r_districtcode::number(18,0) as r_districtcode,
+	r_tahsilcode::number(18,0) as r_tahsilcode,
+	address1::varchar(100) as address1,
 	address2::varchar(100) as address2,
-	retailerchannelcode::varchar(40) as retailerchannelcode,	
-    retailerclassid::number(18,0) as retailerclassid,
-    actv_flg::varchar(1) as actv_flg,
+	retailerchannelcode::varchar(40) as retailerchannelcode,
+	retailerclassid::number(18,0) as retailerclassid,
+	actv_flg::varchar(1) as actv_flg,
 	filename::varchar(100) as filename,
-	crt_dttm::timestamp_ntz(9) as crt_dttm,
-	updt_dttm::timestamp_ntz(9) as updt_dttm
+	current_timestamp()::timestamp_ntz(9) as crt_dttm,
+	current_timestamp()::timestamp_ntz(9) as updt_dttm
 
     from transformed
 )
