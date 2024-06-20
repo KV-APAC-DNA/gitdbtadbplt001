9fdca563c1ab6229ee80e417175308e97d27a769
Chandrakant Dash <CDash6@kenvue.com>
2024-06-20 21:12:10 +0000

    Added Inventory

---
+++ models/asp_regional/aspwks_integration/comex/aspwks_integration__wks_commercial_excellence_ecomm_base.sql
@@ -0,0 +1,31 @@
+with edw_rpt_ecomm_oneview as (
+    select * from {{ source('snapaspedw_integration', 'edw_rpt_ecomm_oneview') }}
+),
+final as ( 
+    select 
+NVL(base_dist.market,'NA') :: varchar(40) AS market, 
+NVL(base_dist."cluster",'NA') :: varchar(100) AS "cluster",
+'ECOM NTS' :: varchar(100) AS kpi,
+base_dist.month_id :: varchar(23) as month_id,
+sum(base.value_usd) :: numeric(38,5) as mtd_usd, sum(base.value_lcy) :: numeric(38,5) as mtd_lcy
+ from (select * from
+(select distinct CASE WHEN market like '%Japan%' THEN 'Japan'
+		WHEN market = 'Pacific' THEN 'Australia'
+        ELSE market END as market,
+	cluster as "cluster" from edw_rpt_ecomm_oneview where kpi = 'NTS')t cross join
+(select distinct
+		left(fisc_yr_per, 4)||right(fisc_yr_per, 2) as month_id
+	from edw_rpt_ecomm_oneview where kpi = 'NTS') u ) base_dist
+left join
+	(select CASE WHEN market like '%Japan%' THEN 'Japan'
+		WHEN market = 'Pacific' THEN 'Australia'
+        ELSE market END as market,
+        cluster as "cluster",
+		left(fisc_yr_per, 4)||right(fisc_yr_per, 2) as month_id,
+		value_usd, value_lcy, kpi
+        from edw_rpt_ecomm_oneview where kpi = 'NTS') base
+on base_dist.market = base.market AND base_dist."cluster" = base."cluster" AND base_dist.month_id = base.month_id
+group by base_dist.market, base_dist."cluster", base_dist.month_id
+
+)
+select * from final 
---
+++ models/asp_regional/aspwks_integration/comex/aspwks_integration__wks_commercial_excellence_gts_base.sql
@@ -0,0 +1,51 @@
+with edw_market_mirror_fact as (
+    select * from {{ source('snapaspedw_integration', 'v_rpt_copa_commercial_excellence_sync') }}
+),
+itg_query_parameters as ( 
+    select * from {{ source('aspitg_integration', 'itg_query_parameters') }}
+),
+itg_mds_ap_sales_ops_map as (
+    select * from {{ ref('aspitg_integration__itg_mds_ap_sales_ops_map') }}
+),
+final as (
+    SELECT market :: varchar(40) as market,
+    "cluster" :: varchar(100) as "cluster",
+    month_id :: varchar(23) as month_id,
+    kpi :: varchar(100) as kpi,
+    kv_val_usd :: numeric(38,5) as kv_val_usd,
+    KV_VAL_LC :: numeric(38,5) as KV_VAL_LC,
+    CAT_VAL_USD :: numeric(38,5) as CAT_VAL_USD,
+    CAT_VAL_LC :: numeric(38,5) as CAT_VAL_LC
+     FROM (
+select CASE
+	WHEN market = 'China' AND trans.supplier = 'IQVIA' THEN 'China Selfcare'
+	WHEN market = 'China' AND trans.supplier <> 'IQVIA' THEN 'China Personal Care'
+	ELSE market_map.destination_market
+END AS market,
+destination_cluster AS "cluster",
+extract(year from time_period)||LPAD(extract(month from time_period),2,'00') AS month_id,
+'MARKET SHARE' AS kpi,
+sum(case when upper(manufacturer) in (SELECT DISTINCT parameter_value AS manufacturer
+	FROM itg_query_parameters
+	WHERE country_code = 'APAC' AND parameter_name = 'price_tracker_mfr')
+	then sku_value_sales_usd else 0 end) as kv_val_usd,
+sum(case when upper(manufacturer) in (SELECT DISTINCT parameter_value AS manufacturer
+	FROM itg_query_parameters
+	WHERE country_code = 'APAC' AND parameter_name = 'price_tracker_mfr')
+	then sku_value_sales_lc else 0 end) as kv_val_lc,
+sum(sku_value_sales_usd) as cat_val_usd,
+sum(sku_value_sales_lc) as cat_val_lc
+FROM edw_market_mirror_fact trans left join itg_mds_ap_sales_ops_map market_map
+ON UPPER(market_map.source_market) = UPPER(trans.ggh_country) AND market_map.dataset = 'Market Share QSD'
+where channel_type = 'Total'
+and date_type = 'Monthly'
+and ggh_region = 'APAC'
+group by 1,2,3,4
+	) WHERE month_id::INT<=(select (extract(year from min(latest_date))  || lpad(extract(month from min(latest_date)),2,'0'))::INT  as mkt_shr_max_dt from(
+	select market, max(time_period) as latest_date from edw_market_mirror_fact
+	where channel_type = 'Total'
+	and date_type = 'Monthly'
+	and ggh_region = 'APAC'
+	group by 1 order by 1))
+)
+select * from final 
