7f9440f1c1f029849e1d162f93b0c9cb64881705
Debipriya Kanungo <dkanun02@kenvue.com>
2024-06-20 19:44:24 +0000

    SG

--- models/asp_regional/aspedw_integration/generic_hierarchy_models/aspedw_integration__edw_generic_product_key_attributes.sql
+++ models/asp_regional/aspedw_integration/generic_hierarchy_models/aspedw_integration__edw_generic_product_key_attributes.sql
@@ -1,5 +1,5 @@
 with EDW_PRODUCT_KEY_ATTRIBUTES as (
-      select * from {{ source('aspedw_integration','edw_product_key_attributes') }}
+     select * from {{ ref('aspedw_integration__edw_product_key_attributes') }}
 
 ),
 
