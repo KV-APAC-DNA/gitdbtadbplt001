bbee23218784c1be050c812a870c6a67ec04b3c6
Debipriya Kanungo <dkanun02@kenvue.com>
2024-06-24 06:30:46 +0000

    re

--- models/asp_regional/aspedw_integration/retail_excellence/aspedw_integration__v_edw_vw_cal_Retail_excellence_dim.sql
+++ models/asp_regional/aspedw_integration/retail_excellence/aspedw_integration__v_edw_vw_cal_Retail_excellence_dim.sql
@@ -3,14 +3,7 @@ with edw_vw_cal_retail_excellence_dim as (
 ),
 EDW_VW_CAL_RETAIL_EXCELLENCE_DIM_trans as
 (
-SELECT to_char((current_timestamp::character varying)::timestamp without time zone, ('YYYYMM'::varchar)::text) AS curr_mnth,		--// SELECT to_char(('now'::character varying)::timestamp without time zone, ('YYYYMM'::character varying)::text) AS curr_mnth, // character varyingmodels
-asp_regional
-aspedw_integration
-retail_excellence
-aspedw_integration__v_edw_vw_cal_Retail_excellence_dim.sql
-
-Save
-987563412
+SELECT to_char((current_timestamp::character varying)::timestamp without time zone, ('YYYYMM'::varchar)::text) AS curr_mnth,		--// SELECT to_char(('now'::character varying)::timestamp without time zone, ('YYYYMM'::character varying)::text) AS curr_mnth, // character varying
  to_char(add_months((current_timestamp::varchar)::timestamp without time zone, (- (1)::bigint)),		--//  to_char(add_months(('now'::character varying)::timestamp without time zone, (- (1)::bigint)), // character varying
  ('YYYYMM'::character varying)::text) AS prev_mnth, to_char(add_months((current_timestamp::varchar)::timestamp without time zone,		--//  ('YYYYMM'::character varying)::text) AS prev_mnth, to_char(add_months(('now'::character varying)::timestamp without time zone, // character varying
  (- (2)::bigint)), ('YYYYMM'::character varying)::text) AS last_2mnths, to_char(add_months((current_timestamp::varchar)::timestamp without time zone,		--//  (- (2)::bigint)), ('YYYYMM'::character varying)::text) AS last_2mnths, to_char(add_months(('now'::character varying)::timestamp without time zone, // character varying
