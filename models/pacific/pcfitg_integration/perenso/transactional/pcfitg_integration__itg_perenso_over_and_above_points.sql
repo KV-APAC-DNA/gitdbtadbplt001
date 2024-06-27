8aa391f8c19a60f59121c0b1ac2509ce635ff27f
Jeeva Ananth C <jc3@kenvue.com>
2024-06-27 09:24:28 +0000

    Added test values in source cte

--- models/pacific/pcfitg_integration/perenso/transactional/pcfitg_integration__itg_perenso_over_and_above_points.sql
+++ models/pacific/pcfitg_integration/perenso/transactional/pcfitg_integration__itg_perenso_over_and_above_points.sql
@@ -1,14 +1,26 @@
 -- REQ000000180060: SSR O&A display points content change
 with source as
 (
-    select * from {{ source('pcfsdl_raw', 'sdl_over_and_above_points') }}
+SELECT 'Full gondola end'AS display_type, 140 AS points UNION ALL
+SELECT 'Half gondola end'AS display_type, 90 AS points UNION ALL
+SELECT 'Full wing'AS display_type, 80 AS points UNION ALL
+SELECT 'Half wing'AS display_type, 50 AS points UNION ALL
+SELECT 'Full shelf'AS display_type, 40 AS points UNION ALL
+SELECT 'Half shelf'AS display_type, 25 AS points UNION ALL
+SELECT 'Dump bin'AS display_type, 50 AS points UNION ALL
+SELECT 'Half dump bin'AS display_type, 25 AS points UNION ALL
+SELECT 'Floor stack'AS display_type, 55 AS points UNION ALL
+SELECT 'Tower'AS display_type, 70 AS points UNION ALL
+SELECT 'Counter unit'AS display_type, 40 AS points UNION ALL
+SELECT 'Hang Cell'AS display_type, 25 AS points UNION ALL
+SELECT 'Clip Strip'AS display_type, 25 AS points
 ),
 final as
 (
 select 
     display_type::varchar(256) as oa_display_type,
     points::number(10,3) as points,
-    run_id::number(14,0) as run_id,
+    NULL::number(14,0) as run_id,
     current_timestamp()::timestamp_ntz(9) as create_dt,
     current_timestamp()::timestamp_ntz(9) as update_dt
 from source
