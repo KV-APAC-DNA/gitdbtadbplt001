26b54a4867189c4f5977e042f31dd7e0ba202eac
Jeeva Ananth C <jc3@kenvue.com>
2024-06-28 05:18:48 +0000

    Changed table to union

--- models/pacific/pcfitg_integration/perenso/transactional/pcfitg_integration__itg_perenso_over_and_above_points.sql
+++ models/pacific/pcfitg_integration/perenso/transactional/pcfitg_integration__itg_perenso_over_and_above_points.sql
@@ -1,20 +1,20 @@
 -- REQ000000180060: SSR O&A display points content change
 with source as
 (
--- SELECT 'Full gondola end'AS display_type, 140 AS points UNION ALL
--- SELECT 'Half gondola end'AS display_type, 90 AS points UNION ALL
--- SELECT 'Full wing'AS display_type, 80 AS points UNION ALL
--- SELECT 'Half wing'AS display_type, 50 AS points UNION ALL
--- SELECT 'Full shelf'AS display_type, 40 AS points UNION ALL
--- SELECT 'Half shelf'AS display_type, 25 AS points UNION ALL
--- SELECT 'Dump bin'AS display_type, 50 AS points UNION ALL
--- SELECT 'Half dump bin'AS display_type, 25 AS points UNION ALL
--- SELECT 'Floor stack'AS display_type, 55 AS points UNION ALL
--- SELECT 'Tower'AS display_type, 70 AS points UNION ALL
--- SELECT 'Counter unit'AS display_type, 40 AS points UNION ALL
--- SELECT 'Hang Cell'AS display_type, 25 AS points UNION ALL
--- SELECT 'Clip Strip'AS display_type, 25 AS points
-SELECT * FROM  SBX_DNA_LAB1.JC3_RAW.pcfsdl_raw__sdl_over_and_above_points 
+SELECT 'Full gondola end'AS display_type, 140 AS points UNION ALL
+SELECT 'Half gondola end'AS display_type, 90 AS points UNION ALL
+SELECT 'Full wing'AS display_type, 80 AS points UNION ALL
+SELECT 'Half wing'AS display_type, 50 AS points UNION ALL
+SELECT 'Full shelf'AS display_type, 40 AS points UNION ALL
+SELECT 'Half shelf'AS display_type, 25 AS points UNION ALL
+SELECT 'Dump bin'AS display_type, 50 AS points UNION ALL
+SELECT 'Half dump bin'AS display_type, 25 AS points UNION ALL
+SELECT 'Floor stack'AS display_type, 55 AS points UNION ALL
+SELECT 'Tower'AS display_type, 70 AS points UNION ALL
+SELECT 'Counter unit'AS display_type, 40 AS points UNION ALL
+SELECT 'Hang Cell'AS display_type, 25 AS points UNION ALL
+SELECT 'Clip Strip'AS display_type, 25 AS points
+--SELECT * FROM  SBX_DNA_LAB1.JC3_RAW.pcfsdl_raw__sdl_over_and_above_points 
 
 ),
 final as
