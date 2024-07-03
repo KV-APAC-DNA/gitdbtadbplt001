d29ff001ac2b98d6b2ec7e193412eeb96932bf35
Sandip Kumar Maharaj <SMahar01@kenvue.com>
2024-07-03 12:19:11 +0000

    Update_Append with Crt_Dttm

--- models/india/inditg_integration/dailysales/master/inditg_integration__itg_dailysales.sql
+++ models/india/inditg_integration/dailysales/master/inditg_integration__itg_dailysales.sql
@@ -75,7 +75,7 @@ final as
     salfreeqtyvalue::number(38,6) as salfreeqtyvalue,
     nrvalue::number(18,6) as nrvalue,
     vcpschemeamount::number(18,6) as vcpschemeamount,
-    current_timestamp()::timestamp_ntz(9) as crt_dttm,
+    crt_dttm::timestamp_ntz(9) as crt_dttm,
     current_timestamp()::timestamp_ntz(9) as updt_dttm,
     current_timestamp()::timestamp_ntz(9) as modifieddate
     from source
--- models/india/inditg_integration/sales_return/master/inditg_integration__itg_salesreturn.sql
+++ models/india/inditg_integration/sales_return/master/inditg_integration__itg_salesreturn.sql
@@ -62,9 +62,9 @@ final as
     salesroutecode::varchar(200) as salesroutecode,
     nrvalue::number(18,6) as nrvalue,
     prdselrateaftertax::number(18,6) as prdselrateaftertax,
-    current_timestamp()::timestamp_ntz(9) as crt_dttm,
+    crt_dttm::timestamp_ntz(9) as crt_dttm,
     current_timestamp()::timestamp_ntz(9) as updt_dttm,
-    current_timestamp()::timestamp_ntz(9) as modifieddate
+    modifieddate::timestamp_ntz(9) as modifieddate
     from source
     {% if is_incremental() %}
     --this filter will only be applied on an incremental run
