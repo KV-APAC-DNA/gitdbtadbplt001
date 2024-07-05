7e53f5808ec3dfff03cc880771a7efd3cd82277b
Niketh Narasimhan <nnaras01@kenvue.com>
2024-07-05 09:26:08 +0000

    ok

--- models/asp_regional/aspwks_integration/pop6/aspwks_integration__wks_pop6_jp_product_lists_allocation.sql
+++ models/asp_regional/aspwks_integration/pop6/aspwks_integration__wks_pop6_jp_product_lists_allocation.sql
@@ -1,6 +1,6 @@
 with sdl_pop6_jp_product_lists_allocation as 
 (
-    select * from {{ source('sgpsdl_raw', 'sdl_pop6_jp_product_lists_allocation') }}
+    select * from {{ source('jpnsdl_raw', 'sdl_pop6_jp_product_lists_allocation') }}
 ),
 final as (
     select * from sdl_pop6_jp_product_lists_allocation
