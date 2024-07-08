a310c796d5a0071c5222976db7f2c4dfc4af07ec
Anjali Agrawal <AAgraw03@kenvue.com>
2024-07-08 07:55:43 +0000

    Fixed code

--- models/malaysia/mysitg_integration/perfect_store/master/mysitg_integration__itg_mds_my_ps_weights.sql
+++ models/malaysia/mysitg_integration/perfect_store/master/mysitg_integration__itg_mds_my_ps_weights.sql
@@ -6,7 +6,7 @@ AS (
 final
 AS (
     SELECT kpi::VARCHAR(100) AS kpi,
-        name::VARCHAR(255) AS name,
+        name::VARCHAR(255) AS retail_env,
         weight::NUMBER(20,4) AS weight,
         channel::VARCHAR(50) AS channel,
         convert_timezone('Asia/Singapore',current_timestamp()::timestamp_ntz(9)) as crtd_dttm
