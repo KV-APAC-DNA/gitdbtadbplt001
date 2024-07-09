724182da94af804e7e97462df83aee3a3cc52fe5
alaksh01 <alaksh01@kenvue.com>
2024-07-09 11:55:21 +0530

    AL: All hcp files

---
+++ models/hcp/hcpitg_integration/rtl/transactional/hcpitg__integration_itg_hcp360_in_ventasys_rtlmaster.sql
@@ -0,0 +1,39 @@
+{{
+    config
+    (
+        materialized="incremental",
+        incremental_strategy= "append",
+        pre_hook = "{% if is_incremental() %}
+        delete from {{this}} WHERE (team_name,v_custid_rtl) IN (SELECT sdl.team_name,sdl.v_custid_rtl
+                                   FROM {{ source('hcpsdl_raw', 'sdl_hcp360_in_ventasys_rtlmaster sdl') }} sdl 
+                                   INNER JOIN itg_hcp360_in_ventasys_rtlmaster  itg
+                                   ON sdl.team_name = itg.team_name
+                                   AND sdl.v_custid_rtl = itg.v_custid_rtl
+                                   )
+        {% endif %}"
+    )
+}}
+with sdl_hcp360_in_ventasys_rtlmaster as(
+    select * from {{ source('hcpsdl_raw', 'sdl_hcp360_in_ventasys_rtlmaster') }}
+),
+final as 
+(
+select
+  team_name::varchar(20) as team_name,
+  v_custid_rtl::varchar(50) as v_custid_rtl,
+  v_terrid::varchar(50) as v_terrid,
+  cust_name::varchar(500) as cust_name,
+  cust_type::varchar(50) as cust_type,
+  is_active::varchar(1) as is_active,
+  cust_endtered_date::date as cust_endtered_date,
+  urc::number(30,0) as urc,
+  crt_dttm::timestamp_ntz(9) as crt_dttm,
+  convert_timezone('UTC', current_timestamp())::timestamp_ntz(9) as updt_dttm,
+  filename::varchar(50) as filename
+from
+  sdl_hcp360_in_ventasys_rtlmaster
+)
+select
+  *
+from
+  final
---
+++ models/hcp/hcpwks_integration/rx_cx/transactional/hcpwks_integration__sales_actual_achnr_qty.sql
@@ -0,0 +1,69 @@
+with 
+edw_dailysales_fact as 
+(
+    select * from {{ ref('indedw_integration__edw_dailysales_fact') }}
+),
+edw_retailer_calendar_dim as 
+(
+    select * from {{ ref('indedw_integration__edw_retailer_calendar_dim') }}
+),
+edw_product_dim as 
+(
+    select * from {{ ref('indedw_integration__edw_product_dim') }}
+),
+edw_retailer_dim as 
+(
+    select * from {{ ref('indedw_integration__edw_retailer_dim') }}
+),
+itg_ventasys_jnj_prod_mapping as
+(
+    select * from snapinditg_integration.itg_ventasys_jnj_prod_mapping
+),
+itg_query_parameters as 
+(
+    select * from {{ source('inditg_integration', 'itg_query_parameters') }}
+),
+trans as 
+(
+    SELECT cal.cal_yr,
+        cal.qtr,
+        mapp.prod_vent,
+        ret.rtruniquecode as urc,
+        sum(sales.achievement_nr_val) as actual_ach_nr,
+        sum(sales.quantity) as actual_qty
+FROM edw_dailysales_fact sales
+  INNER JOIN edw_retailer_calendar_dim cal 
+          ON sales.invoice_date = cal.day
+  INNER JOIN edw_product_dim prod
+          ON sales.product_code = prod.product_code
+  INNER JOIN itg_ventasys_jnj_prod_mapping mapp
+          ON mapp.prod_name = prod.variant_name
+         AND (mapp.prod_vent = 'ORSL (Core)'
+          OR mapp.prod_vent = 'ORSL (Plus)'
+          OR mapp.prod_vent = 'ORSL (Rehydrate)')
+  INNER JOIN edw_retailer_dim ret
+          ON sales.retailer_code = ret.retailer_code
+         AND sales.customer_code = ret.customer_code
+         AND ret.actv_flg = 'Y'
+WHERE cal.fisc_yr > EXTRACT(YEAR FROM current_timestamp()) - (SELECT CAST(PARAMETER_VALUE AS INTEGER) AS PARAMETER_VALUE
+                                                   FROM   itg_query_parameters
+                                                   WHERE  UPPER(COUNTRY_CODE) = 'IN'
+                                                     AND  UPPER(PARAMETER_TYPE) = 'YEAR_RANGE'
+                                                     AND  UPPER(PARAMETER_NAME) = 'IN_RX_CX_DATA_RETENSION_PERIOD') 
+GROUP BY 1,
+         2,
+         3,
+         4
+),
+final as 
+(
+    select 
+	cal_yr::number(18,0) as cal_yr,
+	qtr::number(18,0) as qtr,
+	prod_vent::varchar(200) as prod_vent,
+	urc::varchar(100) as urc,
+	actual_ach_nr::number(38,6) as actual_ach_nr,
+	actual_qty::number(38,0) as actual_qty
+    from trans
+)
+select * from final
