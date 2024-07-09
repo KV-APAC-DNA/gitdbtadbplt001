81292d0a00ded8577a4d61aabf6594118313c6ce
alaksh01 <alaksh01@kenvue.com>
2024-07-09 11:53:44 +0530

    AL: All hcp files

---
+++ models/hcp/hcpwks_integration/rx_cx/transactional/hcpwks_integration__rx_cx_urc_ventasys_rtl.sql
@@ -0,0 +1,34 @@
+with 
+rx_cx_rxrtl_qtly_agg as 
+(
+    select * from {{ ref('hcpwks_integration__rx_cx_rxrtl_qtly_agg') }}
+),
+itg_hcp360_in_ventasys_rtlmaster as 
+(
+    select * from snapinditg_integration.itg_hcp360_in_ventasys_rtlmaster
+), --hcp
+final as 
+(
+    SELECT  
+        rtl.urc,
+        tmp.rx_product,
+        SUM(tmp.rx_units) AS rx_units,
+        tmp.year,
+        tmp.quarter,
+        count(NULLIF(round(rx_units,2),0.00)) AS presc_mnth_cnt
+FROM  rx_cx_rxrtl_qtly_agg tmp
+INNER JOIN (SELECT urc,
+                   v_custid_rtl,
+                   ROW_NUMBER() OVER (PARTITION BY urc ORDER BY v_custid_rtl DESC) AS rnk
+            FROM itg_hcp360_in_ventasys_rtlmaster
+            ) rtl
+        ON tmp.v_custid_rtl = rtl.v_custid_rtl
+       AND rtl.rnk = 1
+WHERE rtl.urc IS NOT NULL 
+  AND TRIM(rtl.urc) <> ''
+GROUP BY rtl.urc,
+         tmp.rx_product,
+         tmp.year,
+         tmp.quarter
+)
+select * from final
