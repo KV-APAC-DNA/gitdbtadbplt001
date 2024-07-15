409e82f59802f851d2a1a70e77f91e3295d9d9d9
SwethaNenavath123 <155425374+SwethaNenavath96@users.noreply.github.com>
2024-07-15 22:36:48 +0530

    added logic so that same file won't get loaded

--- models/asp_regional/aspitg_integration/pop6/master/aspitg_integration__itg_pop6_display_plans.sql
+++ models/asp_regional/aspitg_integration/pop6/master/aspitg_integration__itg_pop6_display_plans.sql
@@ -23,7 +23,7 @@ sdl_pop6_th_display_plans as (
     select * from {{ref('aspwks_integration__wks_pop6_th_display_plans')}}
 ),
 transformed as (
-SELECT  
+SELECT
 	'KR' as cntry_cd,
 	substring(file_name, 1, 8) as src_file_date,
 	display_plan_id,
@@ -46,15 +46,18 @@ SELECT
 	crtd_dttm,
     convert_timezone('Asia/Singapore',current_timestamp())::timestamp_ntz(9) as updt_dttm
 FROM sdl_pop6_kr_display_plans
+    {% if is_incremental() %}
+        where file_name not in (select distinct file_name from {{this}} where cntry_cd='KR')
+    {% endif %}
 union
-SELECT  
+SELECT
 	'TW',
 	substring(file_name, 1, 8),
 	display_plan_id,
 	status,
 	allocation_method,
 	pop_code_or_pop_list_code,
-	team,	
+	team,
 	display_code,
 	display_name,
 	required_number_of_displays,
@@ -70,8 +73,11 @@ SELECT
 	crtd_dttm,
     convert_timezone('Asia/Singapore',current_timestamp())::timestamp_ntz(9)
 FROM sdl_pop6_tw_display_plans
+    {% if is_incremental() %}
+        where file_name not in (select distinct file_name from {{this}} where cntry_cd='TW')
+    {% endif %}
 union
-SELECT  
+SELECT
 	'HK',
 	substring(file_name, 1, 8),
 	display_plan_id,
@@ -94,8 +100,11 @@ SELECT
 	crtd_dttm,
     convert_timezone('Asia/Singapore',current_timestamp())::timestamp_ntz(9)
 FROM sdl_pop6_hk_display_plans
+    {% if is_incremental() %}
+        where file_name not in (select distinct file_name from {{this}} where cntry_cd='HK')
+    {% endif %}
 union
-SELECT  
+SELECT
 	'JP',
 	substring(file_name, 1, 8),
 	display_plan_id,
@@ -118,8 +127,11 @@ SELECT
 	crtd_dttm,
     convert_timezone('Asia/Singapore',current_timestamp())::timestamp_ntz(9)
 FROM sdl_pop6_jp_display_plans
+    {% if is_incremental() %}
+        where file_name not in (select distinct file_name from {{this}} where cntry_cd='JP')
+    {% endif %}
 union
-SELECT  
+SELECT
 	'SG',
 	substring(file_name, 1, 8),
 	display_plan_id,
@@ -142,10 +154,13 @@ SELECT
 	crtd_dttm,
     convert_timezone('Asia/Singapore',current_timestamp())::timestamp_ntz(9)
 FROM sdl_pop6_sg_display_plans
+    {% if is_incremental() %}
+        where file_name not in (select distinct file_name from {{this}} where cntry_cd='SG')
+    {% endif %}
 /*Changes for TH POP6 - Jan 2024*/
 union
 SELECT 'TH',
-       substring(file_name,1,8), 
+       substring(file_name,1,8),
        display_plan_id,
        status,
        allocation_method,
@@ -167,6 +182,9 @@ SELECT 'TH',
        crtd_dttm,
     convert_timezone('Asia/Singapore',current_timestamp())::timestamp_ntz(9)
 FROM sdl_pop6_th_display_plans
+    {% if is_incremental() %}
+        where file_name not in (select distinct file_name from {{this}} where cntry_cd='TH')
+    {% endif %}
 ),
 final as (
 select
@@ -194,7 +212,7 @@ updt_dttm::timestamp_ntz(9) as updt_dttm
 from transformed
 {% if is_incremental() %}
     -- this filter will only be applied on an incremental run
-    where transformed.crtd_dttm > (select max(crtd_dttm) from {{ this }}) 
+    where transformed.crtd_dttm > (select max(crtd_dttm) from {{ this }})
  {% endif %}
 )
-select * from final 
+select * from final
