8667f207a2200c7502f697da06ac9a3265223829
Vasundhara Ujawane <vujawane@kenvue.com>
2024-07-25 09:42:33 +0000

    RIR view changes for KPI calculations

--- models/asp_regional/aspedw_integration/pop6/transactional/aspedw_integration__edw_vw_pop6_visits_rir_data.sql
+++ models/asp_regional/aspedw_integration/pop6/transactional/aspedw_integration__edw_vw_pop6_visits_rir_data.sql
@@ -6,123 +6,100 @@ itg_pop6_executed_visits as
 (
     select * from {{ ref('aspitg_integration__itg_pop6_executed_visits') }}
 ),
-itg_pop6_product_lists_pops as 
+edw_vw_pop6_products as 
 (
-    select * from {{ ref('aspitg_integration__itg_pop6_product_lists_pops') }}
-),
-itg_pop6_product_lists_products as 
-(
-    select * from {{ ref('aspitg_integration__itg_pop6_product_lists_products') }}
+    select * from {{ ref('aspedw_integration__edw_vw_pop6_products') }}
 ),
 visit as 
 (
-    SELECT visit.cntry_cd,
-        visit.src_file_date,
-        visit.visit_id,
-        visit.visit_date,
-        visit.check_in_datetime,
-        visit.check_out_datetime,
-        visit.popdb_id,
-        visit.pop_code,
-        visit.pop_name,
-        visit.address,
-        visit.check_in_longitude,
-        visit.check_in_latitude,
-        visit.check_out_longitude,
-        visit.check_out_latitude,
-        visit.check_in_photo,
-        visit.check_out_photo,
-        visit.username,
-        visit.user_full_name,
-        visit.superior_username,
-        visit.superior_name,
-        visit.planned_visit,
-        visit.cancelled_visit,
-        visit.cancellation_reason,
-        visit.cancellation_note,
-        visit.rn
-    FROM 
-        (
-            SELECT 
-                itg_pop6_executed_visits.cntry_cd,
-                itg_pop6_executed_visits.src_file_date,
-                itg_pop6_executed_visits.visit_id,
-                itg_pop6_executed_visits.visit_date,
-                itg_pop6_executed_visits.check_in_datetime,
-                itg_pop6_executed_visits.check_out_datetime,
-                itg_pop6_executed_visits.popdb_id,
-                itg_pop6_executed_visits.pop_code,
-                itg_pop6_executed_visits.pop_name,
-                itg_pop6_executed_visits.address,
-                itg_pop6_executed_visits.check_in_longitude,
-                itg_pop6_executed_visits.check_in_latitude,
-                itg_pop6_executed_visits.check_out_longitude,
-                itg_pop6_executed_visits.check_out_latitude,
-                itg_pop6_executed_visits.check_in_photo,
-                itg_pop6_executed_visits.check_out_photo,
-                itg_pop6_executed_visits.username,
-                itg_pop6_executed_visits.user_full_name,
-                itg_pop6_executed_visits.superior_username,
-                itg_pop6_executed_visits.superior_name,
-                itg_pop6_executed_visits.planned_visit,
-                itg_pop6_executed_visits.cancelled_visit,
-                itg_pop6_executed_visits.cancellation_reason,
-                itg_pop6_executed_visits.cancellation_note,
-                row_number() OVER(
-                    PARTITION BY itg_pop6_executed_visits.visit_id,
-                    itg_pop6_executed_visits.popdb_id
-                    ORDER BY itg_pop6_executed_visits.run_id DESC
-                ) AS rn
-            FROM itg_pop6_executed_visits
-            WHERE 
-                (
-                        (itg_pop6_executed_visits.cntry_cd)::text = 'TH'::text
-                    AND 
-                        itg_pop6_executed_visits.visit_id IN (
-                            SELECT DISTINCT itg_pop6_rir_data.visit_id
-                            FROM itg_pop6_rir_data
-                        )
-                )
-        ) visit
-    WHERE (visit.rn = 1)
+   SELECT visit.cntry_cd,
+       visit.src_file_date,
+       visit.visit_id,
+       visit.visit_date,
+       visit.check_in_datetime,
+       visit.check_out_datetime,
+       visit.popdb_id,
+       visit.pop_code,
+       visit.pop_name,
+       visit.address,
+       visit.check_in_longitude,
+       visit.check_in_latitude,
+       visit.check_out_longitude,
+       visit.check_out_latitude,
+       visit.check_in_photo,
+       visit.check_out_photo,
+       visit.username,
+       visit.user_full_name,
+       visit.superior_username,
+       visit.superior_name,
+       visit.planned_visit,
+       visit.cancelled_visit,
+       visit.cancellation_reason,
+       visit.cancellation_note,
+       visit.rn
+FROM (SELECT cntry_cd,
+             src_file_date,
+             visit_id,
+             visit_date,
+             check_in_datetime,
+             check_out_datetime,
+             popdb_id,
+             pop_code,
+             pop_name,
+             address,
+             check_in_longitude,
+             check_in_latitude,
+             check_out_longitude,
+             check_out_latitude,
+             check_in_photo,
+             check_out_photo,
+             username,
+             user_full_name,
+             superior_username,
+             superior_name,
+             planned_visit,
+             cancelled_visit,
+             cancellation_reason,
+             cancellation_note,
+             row_number() OVER (PARTITION BY visit_id,popdb_id ORDER BY run_id DESC) AS rn
+      FROM itg_pop6_executed_visits where cntry_cd = 'TH'  
+      ) visit
+WHERE visit.rn = 1
 ),
 final as
 (    
-    SELECT 
-        visit.cntry_cd,
-        visit.visit_id,
-        visit.visit_date,
-        visit.check_in_datetime,
-        visit.check_out_datetime,
-        visit.popdb_id,
-        visit.pop_code,
-        visit.pop_name,
-        visit.address,
-        visit.check_in_longitude,
-        visit.check_in_latitude,
-        visit.check_out_longitude,
-        visit.check_out_latitude,
-        visit.check_in_photo,
-        visit.check_out_photo,
-        visit.username,
-        visit.user_full_name,
-        visit.superior_username,
-        visit.superior_name,
-        visit.planned_visit,
-        visit.cancelled_visit,
-        visit.cancellation_reason,
-        visit.cancellation_note,
-        plpops.product_list,
-        plprods.productdb_id,
-        plprods.sku
-    FROM 
-    visit,
-        itg_pop6_product_lists_pops plpops,
-        itg_pop6_product_lists_products plprods
-    WHERE upper((plpops.popdb_id)::text) = upper((visit.popdb_id)::text)
-    AND plpops.prod_grp_date = visit.visit_date
-    AND plprods.prod_grp_date = visit.visit_date
-    AND upper((plprods.product_list)::text) = upper((plpops.product_list)::text)
-    AND upper((plpops.product_list)::text) LIKE '%MSL%'::text
+    select rir.cntry_cd,
+        rir.visit_id,
+        rir.sku_id,
+        visit.visit_date, 
+	   visit.check_in_datetime, 
+	   visit.check_out_datetime, 
+	   visit.popdb_id, 
+	   visit.pop_code, 
+	   visit.pop_name, 
+	   visit.address, 
+	   visit.check_in_longitude, 
+	   visit.check_in_latitude, 
+	   visit.check_out_longitude, 
+	   visit.check_out_latitude, 
+	   visit.check_in_photo, 
+	   visit.check_out_photo, 
+	   visit.username, 
+	   visit.user_full_name,
+	   visit.superior_username, 
+	   visit.superior_name, 
+	   visit.planned_visit, 
+	   visit.cancelled_visit, 
+	   visit.cancellation_reason, 
+	   visit.cancellation_note ,
+       prd.company,
+       prd.ps_category,
+       prd.ps_segment,
+       rir.layer,
+       rir.total_layer,
+       rir.facing_of_this_layer
+ from itg_pop6_rir_data rir
+ left join visit on visit.visit_id = rir.visit_id
+ left join edw_vw_pop6_products prd on prd.productdb_id = rir.sku_id
 )
 select * from final
