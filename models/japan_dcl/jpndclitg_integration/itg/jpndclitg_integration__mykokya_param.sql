36f3ed43a1ae161e609c97a60072c3c16777355a
amahin01@kenvue.com <amahin01@kenvue.com>
2024-07-26 12:06:49 +0000

    created model

---
+++ models/japan_dcl/jpndclitg_integration/itg/jpndclitg_integration__mykokya_param.sql
@@ -0,0 +1,46 @@
+{{
+    config(
+        materialized="incremental",
+        incremental_strategy= "append",
+        pre_hook='{{build_mykokya_param()}}'
+    )
+}}
+
+with mykokya_param as (
+    select * from DEV_DNA_LOAD.SNAPJPDCLSDL_RAW.MYKOKYA_PARAM
+),
+
+last_file_id as (
+    select max(file_id) as last_file_id
+    from {{ this }}
+),
+
+transformed as (
+
+    select
+        filename,
+        purpose_type,
+        upload_by,
+        customer_no_type,
+        'Src_File_Dt' as source_file_date,
+        (
+            select last_file_id + 1
+            from last_file_id
+        ) as next_file_id
+    from mykokya_param
+),
+final as
+(
+    select 
+    next_file_id::number(18,0)  as file_id,
+    filename::varchar(100) as filename,
+    purpose_type::varchar(30) as purpose_type,
+    upload_by::varchar(50) as upload_by,
+    customer_no_type::varchar(30) as customer_no_type,
+    source_file_date::varchar(30) as source_file_date,
+    TO_CHAR(CONVERT_TIMEZONE('UTC', 'Asia/Tokyo', CAST(CURRENT_TIMESTAMP() AS TIMESTAMP_NTZ(9))), 'MM-DD-YYYY')::varchar(10) as upload_dt,
+    TO_CHAR(CONVERT_TIMEZONE('UTC', 'Asia/Tokyo', CAST(CURRENT_TIMESTAMP() AS TIMESTAMP_NTZ(9))), 'hh24:mi:ss')::varchar(8) as upload_time
+    from
+    transformed
+)
+select * from final
