0b4c2e5483e34b8bb464b856ef6d706e33c1ab63
eswara praneeth Chithathuri <echithat@kenvue.com>
2024-08-02 09:28:08 +0000

    TW changes

---
+++ models/north_asia/ntawks_integration/retail_excellence/taiwan/ntawks_integration__wks_tw_re_act_l6m.sql
@@ -0,0 +1,32 @@
+--import cte     
+with wks_tw_re_allmonths as (
+    select * from {{ ref('ntawks_integration__wks_tw_re_allmonths') }}
+),
+TW_RE_ACT_L6M as (			 
+SELECT SO.CNTRY_CD,
+       SO.sellout_dim_key,
+       MONTH,
+       so_sls_value,
+       SUM(SO_SLS_QTY) OVER (PARTITION BY cntry_cd,sellout_dim_key ORDER BY month ROWS BETWEEN 5 PRECEDING AND CURRENT ROW) AS L6M_SALES_QTY,
+       SUM(SO_SLS_VALUE) OVER (PARTITION BY cntry_cd,sellout_dim_key ORDER BY month ROWS BETWEEN 5 PRECEDING AND CURRENT ROW) AS L6M_SALES,
+       AVG(SO_SLS_QTY) OVER (PARTITION BY cntry_cd,sellout_dim_key ORDER BY month ROWS BETWEEN 5 PRECEDING AND CURRENT ROW) AS L6M_AVG_SALES_QTY,
+       SUM(SALES_VALUE_LIST_PRICE) OVER (PARTITION BY CNTRY_CD,SELLOUT_DIM_KEY ORDER BY MONTH ROWS BETWEEN 5 PRECEDING AND CURRENT ROW) AS L6M_SALES_LP
+FROM WKS_TW_RE_ALLMONTHS SO
+--WHERE sellout_dim_key in ( '105d8348b3394ef5bd2ff7986c950b91')
+ORDER BY SO.CNTRY_CD,
+         SO.sellout_dim_key,
+         MONTH
+),
+final as (
+select
+cntry_cd::varchar(2) AS cntry_cd,
+sellout_dim_key::varchar(32) AS sellout_dim_key,
+month::varchar(23) AS month,
+so_sls_value::numeric(38,6) AS so_sls_value,
+l6m_sales_qty::numeric(38,6) AS l6m_sales_qty,
+l6m_sales::numeric(38,6) AS l6m_sales,
+l6m_avg_sales_qty::numeric(38,6) AS l6m_avg_sales_qty,
+l6m_sales_lp::numeric(38,12) AS l6m_sales_lp
+from TW_RE_ACT_L6M
+)
+select * from final	
