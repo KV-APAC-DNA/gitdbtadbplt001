22e5087a55862b246b15e2eebe048d593a6add7b
Ashika Singh <ASing012@kenvue.com>
2024-08-05 07:29:31 +0000

    Modified code

--- models/hcp/hcpitg_integration/rtl/transactional/hcpitg_integration__itg_hcp360_in_ventasys_hcprtl.sql
+++ models/hcp/hcpitg_integration/rtl/transactional/hcpitg_integration__itg_hcp360_in_ventasys_hcprtl.sql
@@ -10,18 +10,19 @@
     )
 }}
 with sdl_hcp360_in_ventasys_hcprtl as (
-    select * from {{ source('hcpsdl_raw', 'sdl_hcp360_in_ventasys_hcprtl') }}
+    select dense_rank() over (partition by team_name, v_custid_dr, v_custid_rtl order by filename desc ) as rnk from {{ source('hcpsdl_raw', 'sdl_hcp360_in_ventasys_hcprtl') }}
 ),
 final as (
+    select * from (
     select
         team_name::varchar(20) as team_name,
         v_custid_dr::varchar(50) as v_custid_dr,
         v_custid_rtl::varchar(50) as v_custid_rtl,
-        crt_dttm::timestamp_ntz(9) as crt_dttm,
+        crt_dttm,
         convert_timezone('UTC', current_timestamp())::timestamp_ntz(9) as updt_dttm,
         filename::varchar(50) as filename
     from
-        sdl_hcp360_in_ventasys_hcprtl
+        sdl_hcp360_in_ventasys_hcprtl ) where rnk=1
 )
 select * from final
 
