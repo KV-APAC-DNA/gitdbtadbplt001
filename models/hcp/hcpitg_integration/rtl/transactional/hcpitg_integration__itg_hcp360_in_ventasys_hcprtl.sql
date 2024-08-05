ca47222d600ea9e3d5f97bbd408422147e48d4f1
Ashika Singh <ASing012@kenvue.com>
2024-08-05 07:42:56 +0000

    Modified code

--- models/hcp/hcpitg_integration/rtl/transactional/hcpitg_integration__itg_hcp360_in_ventasys_hcprtl.sql
+++ models/hcp/hcpitg_integration/rtl/transactional/hcpitg_integration__itg_hcp360_in_ventasys_hcprtl.sql
@@ -17,7 +17,7 @@ final as (
         team_name::varchar(20) as team_name,
         v_custid_dr::varchar(50) as v_custid_dr,
         v_custid_rtl::varchar(50) as v_custid_rtl,
-        crt_dttm::timestamp_ntz(9) as crt_dttm,,
+        crt_dttm::timestamp_ntz(9) as crt_dttm,
         convert_timezone('UTC', current_timestamp())::timestamp_ntz(9) as updt_dttm,
         filename::varchar(50) as filename
     from
