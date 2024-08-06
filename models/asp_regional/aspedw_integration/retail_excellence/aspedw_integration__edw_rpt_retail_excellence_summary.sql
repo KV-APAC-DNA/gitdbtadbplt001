093b99d78030ae91773bc106ba6d998ade171a42
Anjali Makhloga <amakhlog@kenvue.com>
2024-08-06 12:16:26 +0000

    RE - China included in summary details

--- models/asp_regional/aspedw_integration/retail_excellence/aspedw_integration__edw_rpt_retail_excellence_summary.sql
+++ models/asp_regional/aspedw_integration/retail_excellence/aspedw_integration__edw_rpt_retail_excellence_summary.sql
@@ -30,9 +30,15 @@ edw_anz_rpt_retail_excellence_summary as (
 edw_jp_rpt_retail_excellence_summary as (
     select * from {{ ref('aspedw_integration__edw_jp_rpt_retail_excellence_summary') }}
 ),
+edw_cnsc_rpt_retail_excellence_summary as (
+    select * from {{ ref('aspedw_integration__edw_cnsc_rpt_retail_excellence_summary') }}
+),
+edw_cnpc_rpt_retail_excellence_summary as (
+    select * from {{ ref('aspedw_integration__edw_cnpc_rpt_retail_excellence_summary') }}
+),
+
 edw_rpt_retail_excellence_summary as 
 (
-
 SELECT * FROM edw_id_rpt_retail_excellence_summary UNION
 SELECT * FROM edw_my_rpt_retail_excellence_summary UNION
 SELECT * FROM edw_ph_rpt_retail_excellence_summary UNION
@@ -41,6 +47,8 @@ SELECT * FROM edw_sg_rpt_retail_excellence_summary UNION
 SELECT * FROM edw_th_rpt_retail_excellence_summary UNION
 SELECT * FROM edw_in_rpt_retail_excellence_summary UNION
 SELECT * FROM edw_anz_rpt_retail_excellence_summary UNION
-SELECT * FROM edw_jp_rpt_retail_excellence_summary 
+SELECT * FROM edw_jp_rpt_retail_excellence_summary UNION
+SELECT * FROM edw_cnsc_rpt_retail_excellence_summary UNION  
+SELECT * FROM edw_cnpc_rpt_retail_excellence_summary
 )
 select * from edw_rpt_retail_excellence_summary
