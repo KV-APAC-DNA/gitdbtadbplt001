b182f9e760968e93dc8e9b84c297612233f4d344
Anjali Agrawal <AAgraw03@kenvue.com>
2024-08-08 07:02:47 +0000

    fix for limiting OTC data

--- models/north_asia/ntawks_integration/sellout/ntawks_integration__wks_sdl_kr_otc_sellout.sql
+++ models/north_asia/ntawks_integration/sellout/ntawks_integration__wks_sdl_kr_otc_sellout.sql
@@ -1,7 +1,11 @@
 with source as (
     select * from {{ source('ntasdl_raw', 'sdl_kr_otc_sellout') }}
 ),
+itg_kr_gt_sellout as (
+    select * from {{ source('ntaitg_integration', 'itg_kr_gt_sellout') }}
+)
 final as (
     select * from source
+    where to_timestamp(SPLIT_PART(SPLIT_PART(source.FILE_NAME,'_',4),'.',1),'YYYYMMDDHH24MISS')>(select max(updt_dttm) from itg_kr_gt_sellout where dstr_cd='OTC')
 )
 select * from final
