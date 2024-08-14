297b0dcf0e6486f6e0bc9352c9a0b3f5b9434e23
Pranav Pahilwan <PPahil01@kenvue.com>
2024-08-14 07:53:48 +0000

    m_dcl_edw_job1_fix

--- macros/japan_dcl/build_wk_nayol_temp.sql
+++ macros/japan_dcl/build_wk_nayol_temp.sql
@@ -1,7 +1,7 @@
 {% macro build_wk_nayol_temp() %}
     {% set tablename %}
     {% if target.name=='prod' %}
-                jpndcledw_integration.wk_nayol_temp
+                jpdcledw_integration.wk_nayol_temp
             {% else %}
                 {{schema}}.jpndcledw_integration__wk_nayol_temp
     {% endif %}
@@ -9,7 +9,7 @@
     {% set query %}
         CREATE TABLE if not exists
         {% if target.name=='prod' %}
-                    jpndcledw_integration.wk_nayol
+                    jpdcledw_integration.wk_nayol
                 {% else %}
                     {{schema}}.jpndcledw_integration__wk_nayol
                 {% endif %}
@@ -24,7 +24,7 @@
     );
         create or replace table {{tablename}} clone
         {% if target.name=='prod' %}
-            jpndcledw_integration.wk_nayol
+            jpdcledw_integration.wk_nayol
         {% else %}
             {{schema}}.jpndcledw_integration__wk_nayol
         {% endif %};
