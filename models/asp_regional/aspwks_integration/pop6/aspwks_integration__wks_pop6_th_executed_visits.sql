9a407fcda293c01ff2e7aa30c006c6a8ffbd14a1
Vasundhara Ujawane <vujawane@kenvue.com>
2024-08-16 12:54:23 +0000

    changes to create the models

--- models/asp_regional/aspwks_integration/pop6/aspwks_integration__wks_pop6_th_executed_visits.sql
+++ models/asp_regional/aspwks_integration/pop6/aspwks_integration__wks_pop6_th_executed_visits.sql
@@ -3,6 +3,7 @@ source as
 (
     select * from {{ source('thasdl_raw', 'sdl_pop6_th_executed_visits') }}
 ),
+
 final as
 (
     select * from source
--- models/asp_regional/aspwks_integration/pop6/aspwks_integration__wks_pop6_th_sku_audits.sql
+++ models/asp_regional/aspwks_integration/pop6/aspwks_integration__wks_pop6_th_sku_audits.sql
@@ -3,6 +3,7 @@ source as
 (
     select * from {{ source('thasdl_raw', 'sdl_pop6_th_sku_audits') }}
 ),
+
 final as
 (
     select * from source
