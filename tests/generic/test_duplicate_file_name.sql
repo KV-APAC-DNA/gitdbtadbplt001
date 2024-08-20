9c7c142efd677cf52c6a27a71422d34e2ff4989b
Sourav Shukla <sshukl02@kenvue.com>
2024-08-20 09:58:13 +0000

    2 files added

--- tests/generic/test_duplicate_file_name.sql
+++ tests/generic/test_duplicate_file_name.sql
@@ -11,25 +11,6 @@
         with grouped_by as(
             select 
                 'Duplicate records present' AS failure_reason,
-                {% set file_name_columns = [
-                'CDL_SOURCE_FILE', 'FILE_NM', 'SOURCE_FILE_NAME', 'FILENAME',
-                    'file_name', 'filename', 'SRC_FILE', 'LOAD_FILE_NM', 'FILE_NAME', 'src_file'
-                ] | map('lower') | list %}
-                -- Get the actual columns in the model and convert to lowercase
-                {% set actual_columns = adapter.get_columns_in_relation(model) | map(attribute='name') | map('lower')|list  %}
-                {% set reversed_columns = adapter.get_columns_in_relation(model) | map(attribute='name') | map('lower')|reverse  %}
-                -- Log the actual columns and file name columns to debug
-                {{ log('Actual Columns: ' ~ actual_columns, info=True) }}
-                {{ log('File Name Columns: ' ~ file_name_columns, info=True) }}
-                {{ log('File Name Columns_reversed: ' ~ reversed_columns, info=True) }}
-                -- Loop through file_name_columns to find the first matching column in actual_columns
-                {%- for col in reversed_columns %}
-                    {% if col in file_name_columns%}
-                        {{ col }} as file_name
-                    {% if select_columns or not_null_columns %},{% endif %}
-                    {% break %}
-                    {% endif %}
-                {%- endfor %}
                 {%- for item in group_by_columns %}
                     coalesce(upper(trim({{item}})),'NA') as {{item}}
                     {%- if not loop.last -%},
@@ -47,7 +28,6 @@
                 where {{filter}} 
             {% endif %}
             group by 
-            file_name,
             {% for item in group_by_columns -%}
                 coalesce(upper(trim({{item}})),'NA') 
             {%- if not loop.last -%},
