3e4f74ae9f894c3d23afc7a948cb8f725547161b
Niketh Narasimhan <nnaras01@kenvue.com>
2024-08-20 03:04:00 +0000

    added datatypes and ymls

--- models/hcposea/hcposeedw_integration/country/master/hcposeedw_integration__dim_country.sql
+++ models/hcposea/hcposeedw_integration/country/master/hcposeedw_integration__dim_country.sql
@@ -2,23 +2,24 @@ with itg_country as (
 select * from DEV_DNA_CORE.HCPOSEITG_INTEGRATION.ITG_COUNTRY
 ),
 final as (
-SELECT COUNTRY_KEY,
-       COUNTRY_CODE,
-       COUNTRY_NAME,
-       COUNTRY_DISPLAY_ORDER,
-       COUNTRY_GROUP_CODE,
-       COUNTRY_GROUP_NAME,
-       COUNTRY_GROUP_DISPLAY_ORDER,
-       DESCRIPT,
-       ATTR1,
-       ATTR2,
-       ATTR3,
-       ATTR4,
-       ATTR5,
-       MANUAL_UPDATE_DATE,
-       MANUAL_UPDATE_USER,
-       SYSDATE(),
-       NULL
+SELECT 
+country_key as country_key,
+country_code as country_code,
+country_name as country_name,
+country_display_order as country_display_order,
+country_group_code as country_group_code,
+country_group_name as country_group_name,
+country_group_display_order as country_group_display_order,
+descript as description,
+attr1 as attr1,
+attr2 as attr2,
+attr3 as attr3,
+attr4 as attr4,
+attr5 as attr5,
+manual_update_date as manual_update_date,
+manual_update_user as manual_update_user,
+sysdate() as inserted_date,
+null as updated_date
 FROM itg_country
 )
 select * from final
