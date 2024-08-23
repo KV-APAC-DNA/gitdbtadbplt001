97cfef70c45bfd4e0e460b0baa8acf0a9121fb79
Pranavpahilwan26 <PPahil01@kenvue.com>
2024-08-23 12:38:23 +0530

    c1

--- models/thailand/thaitg_integration/inventory/raw/thaitg_integration__sdl_raw_th_dms_inventory_fact.sql
+++ models/thailand/thaitg_integration/inventory/raw/thaitg_integration__sdl_raw_th_dms_inventory_fact.sql
@@ -6,16 +6,12 @@
         ) 
     }}
 WITH source AS (
-        SELECT *,
-            dense_rank() OVER (
-                PARTITION BY dstrbtr_grp_cd,
-                invoice_dt ORDER BY file_name DESC
-                ) AS rnk
+        SELECT *
         FROM {{ source('thasdl_raw', 'sdl_th_dms_inventory_fact') }} source
         WHERE file_name NOT IN (
                 SELECT DISTINCT file_name
                 FROM {{ source('thawks_integration', 'TRATBL_sdl_th_dms_inventory_fact__test_date_format') }}
-                ) qualify rnk = 1
+                )
         ),
     final AS (
         SELECT *
