7818087f79adae2664191cb1e59f12e5160aedf8
Pranavpahilwan26 <PPahil01@kenvue.com>
2024-08-23 12:26:37 +0530

    change

--- models/thailand/thaitg_integration/inventory/raw/thaitg_integration__sdl_raw_th_dms_inventory_fact.sql
+++ models/thailand/thaitg_integration/inventory/raw/thaitg_integration__sdl_raw_th_dms_inventory_fact.sql
@@ -1,17 +1,32 @@
-{{
-    config(
-        materialized="incremental",
-        incremental_strategy="append"
-    )}}
 
-with source as(
-    select * from {{ source('thasdl_raw', 'sdl_th_dms_inventory_fact') }}
-),
-final as(
-    select * from source
-     {% if is_incremental() %}
-    -- this filter will only be applied on an incremental run
-    where curr_date > (select max(curr_date) from {{ this }}) 
- {% endif %}
-)
-select * from final
+{{ 
+    config(
+        materialized = "incremental", 
+        incremental_strategy = "append"
+        ) 
+    }}
+WITH source AS (
+        SELECT *,
+            dense_rank() OVER (
+                PARTITION BY dstrbtr_grp_cd,
+                invoice_dt ORDER BY file_name DESC
+                ) AS rnk
+        FROM {{ source('thasdl_raw', 'sdl_th_dms_inventory_fact') }} source
+        WHERE file_name NOT IN (
+                SELECT DISTINCT file_name
+                FROM {{ source('thawks_integration', 'TRATBL_sdl_th_dms_inventory_fact__test_date_format') }}
+                ) qualify rnk = 1
+        ),
+    final AS (
+        SELECT *
+        FROM source { % IF is_incremental() % }
+            -- this filter will only be applied on an incremental run
+            WHERE curr_date > (
+                SELECT max(curr_date)
+                FROM {{ this }}
+                ) { % endif % } )
+
+
+         SELECT *
+            FROM final
+    
