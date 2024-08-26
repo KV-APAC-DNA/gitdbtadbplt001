756714e1a3a009a6639503996402ceb11ebba52f
Sathya M <SM05@kenvue.com>
2024-08-26 14:49:32 +0000

    sellin fix

--- models/japan/jpnitg_integration/sellin/master/jpnitg_integration__bw_ohjapan_pan_stg.sql
+++ models/japan/jpnitg_integration/sellin/master/jpnitg_integration__bw_ohjapan_pan_stg.sql
@@ -1,5 +1,5 @@
 with itg_copa_trans as(
-	select * from {{ ref('aspitg_integration__itg_copa_trans') }} 
+	select *,coalesce(cust_num,'') as cust_num_upd, coalesce(matl,'') as matl_upd, coalesce(dstr_chnl,'') as dstr_chnl_upd, coalesce(sls_grp,'') as SALES_GRP_upd, coalesce(cust_sls,'') as cust_sls_upd, coalesce(sls_org,'') as sls_org_upd, coalesce(plnt,'') as plnt_upd from {{ ref('aspitg_integration__itg_copa_trans') }} 
 ),
 edw_account_dim as(
 	select * from {{ ref('aspedw_integration__edw_account_dim') }} 
@@ -18,15 +18,15 @@ union1 as(
     rtrim(A.co_cd) as COMP_CODE,
     rtrim(A.crncy_key_trans_crncy) as CURKEY_TC,
     rtrim(A.crncy_key) as CURRENCY,
-    rtrim(A.cust_num) as CUSTOMER,
-    rtrim(A.cust_sls) as CUST_SALES,
-    rtrim(A.dstr_chnl) as DISTR_CHAN,
+    rtrim(A.cust_num_upd) as CUSTOMER,
+    rtrim(A.cust_sls_upd) as CUST_SALES,
+    rtrim(A.dstr_chnl_upd) as DISTR_CHAN,
     SUBSTRING(rtrim(A.caln_yr_mo), 1, 4) || 0 || SUBSTRING(rtrim(A.caln_yr_mo), 5, 6) as FISCPER,
     rtrim(A.fisc_yr_var) as FISCVARNT,
-    rtrim(A.matl) as MATERIAL,
+    rtrim(A.matl_upd) as MATERIAL,
     rtrim(A.obj_crncy_co_obj) as OBJ_CURR,
     '0' as RECORDTP,
-    rtrim(A.sls_grp) as SALES_GRP,
+    rtrim(A.SALES_GRP_upd) as SALES_GRP,
     (0 || (A.val_type_rpt)) as VTYPE,
     SUM(rtrim(A.amt_cntl_area_crncy)) as AMOCAC,
     SUM(round(rtrim(A.amt_obj_crncy))) as AMOCCC,
@@ -36,23 +36,23 @@ union1 as(
     rtrim(D.bravo_acct_l3) as S005_0ACCOUNT,
     rtrim(D.bravo_acct_l4) as S006_0ACCOUNT,
     rtrim(D.bravo_acct_l5) as S007_0ACCOUNT,
-    rtrim(A.plnt) as plnt-- added this column as part of Kizuna phase 2 DCL Integration to identify plants
+    rtrim(A.plnt_upd) as plnt-- added this column as part of Kizuna phase 2 DCL Integration to identify plants
 	FROM itg_copa_trans A,
 	 edw_account_dim d 
    WHERE rtrim(co_cd) IN (
 		SELECT parameter_value
-		FROM itg_query_parameters
+		FROM aspitg_integration.itg_query_parameters
 		WHERE parameter_name = 'company_code_filter_Kizuna'
 		) --hard coded values paramterised
-	AND (A.dstr_chnl  IN (select rtrim(parameter_value) from aspitg_integration.itg_query_parameters where rtrim(parameter_name)='dstr_chnl_filter_Kizuna_phase2') or A.dstr_chnl is null)--hard coded values paramterised
+	AND A.dstr_chnl_upd  IN (select rtrim(parameter_value) from aspitg_integration.itg_query_parameters where rtrim(parameter_name)='dstr_chnl_filter_Kizuna_phase2') --hard coded values paramterised
 	AND (
-		A.cust_num = '' or A.cust_num is null
-		OR A.matl = '' or A.matl is null
+		rtrim(A.cust_num_upd)  = ''  
+		OR rtrim(A.matl_upd) = '' 
 		)
 	AND rtrim(A.amt_obj_crncy) = rtrim(A.grs_amt_trans_crncy)
 	AND rtrim(A.fisc_yr) IN (
 		SELECT cast(right(identify_value, 4) AS INTEGER) AS fisc_yr
-		FROM MT_CONSTANT
+		FROM jpnedw_integration.MT_CONSTANT
 		WHERE identify_cd = 'JCP_PAN_FLG'
 		)
 	AND rtrim(A.acct_num) = rtrim(D.ACCT_NUM)
@@ -64,23 +64,23 @@ union1 as(
     rtrim(A.co_cd),
     rtrim(A.crncy_key_trans_crncy),
     rtrim(A.crncy_key),
-    rtrim(A.cust_num),
-    rtrim(A.cust_sls),
-    rtrim(A.dstr_chnl),
+    rtrim(A.cust_num_upd),
+    rtrim(A.cust_sls_upd),
+    rtrim(A.dstr_chnl_upd),
     rtrim(A.caln_yr_mo),
     rtrim(A.fisc_yr_var),
-    rtrim(A.matl),
+    rtrim(A.matl_upd),
     rtrim(A.obj_crncy_co_obj),
     rtrim(RECORDTP),
-    rtrim(A.sls_grp),
-    rtrim(A.sls_org),
+    rtrim(A.SALES_GRP_upd),
+    rtrim(A.sls_org_upd),
     (VTYPE),
     rtrim(D.bravo_acct_l1),
     rtrim(D.bravo_acct_l2),
     rtrim(D.bravo_acct_l3),
     rtrim(D.bravo_acct_l4),
     rtrim(D.bravo_acct_l5),
-    rtrim(A.plnt)
+    rtrim(A.plnt_upd)
 	
 ),
 union2 as(
