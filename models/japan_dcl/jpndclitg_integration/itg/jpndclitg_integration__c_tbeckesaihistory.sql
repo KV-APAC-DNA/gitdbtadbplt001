933bfc6b16a77aa8738877c1eca614b9f3c6c2fc
Amit Joshi <AJoshi03@kenvue.com>
2024-08-30 10:56:25 +0000

    fixed source file date column

--- models/japan_dcl/jpndclitg_integration/itg/jpndclitg_integration__c_tbeckesaihistory.sql
+++ models/japan_dcl/jpndclitg_integration/itg/jpndclitg_integration__c_tbeckesaihistory.sql
@@ -137,11 +137,11 @@ final as(
         c_dsgmotransactionid::varchar(16) as c_dsgmotransactionid,
         c_dsgmoauthprckbn::varchar(3) as c_dsgmoauthprckbn,
         c_dsgmotransactionsts::varchar(3) as c_dsgmotransactionsts,
-        source_file_date::varchar(10) as source_file_date,
+        null::varchar(10) as source_file_date,
 		current_timestamp()::timestamp_ntz(9) as inserted_date,
-		inserted_by::varchar(100) as inserted_by,
+		null::varchar(100) as inserted_by,
 		current_timestamp()::timestamp_ntz(9) as updated_date,
-		updated_by::varchar(100) as updated_by
+		null::varchar(100) as updated_by
     from source
 )
 select * from final
