c7b5102b01fd692eee25fe20170848f59ca41691
Anjali Makhloga <amakhlog@kenvue.com>
2024-09-06 14:00:14 +0000

    including HK, VN and TW in final summary and details table

--- models/asp_regional/aspedw_integration/retail_excellence/aspedw_integration__edw_rpt_retail_excellence_summary.sql
+++ models/asp_regional/aspedw_integration/retail_excellence/aspedw_integration__edw_rpt_retail_excellence_summary.sql
@@ -36,9 +36,14 @@ edw_cnsc_rpt_retail_excellence_summary as (
 edw_cnpc_rpt_retail_excellence_summary as (
     select * from {{ ref('aspedw_integration__edw_cnpc_rpt_retail_excellence_summary') }}
 ),
-
 edw_hk_rpt_retail_excellence_summary as (
     select * from {{ ref('aspedw_integration__edw_hk_rpt_retail_excellence_summary') }}
+),
+edw_tw_rpt_retail_excellence_summary as (
+    select * from {{ ref('aspedw_integration__edw_tw_rpt_retail_excellence_summary') }}
+),
+edw_vn_rpt_retail_excellence_summary as (
+    select * from {{ ref('aspedw_integration__edw_vn_rpt_retail_excellence_summary') }}
 ),
 
 edw_rpt_retail_excellence_summary as 
@@ -54,62 +59,8 @@ SELECT * FROM edw_anz_rpt_retail_excellence_summary UNION
 SELECT * FROM edw_jp_rpt_retail_excellence_summary UNION
 SELECT * FROM edw_cnsc_rpt_retail_excellence_summary UNION  
 SELECT * FROM edw_cnpc_rpt_retail_excellence_summary UNION
-SELECT * FROM edw_hk_rpt_retail_excellence_summary
-)
-select * from edw_rpt_retail_excellence_summary
-
-{{
-    config(
-        sql_header="USE WAREHOUSE "+ env_var("DBT_ENV_CORE_DB_MEDIUM_WH")+ ";"
-    )
-}}
-with edw_id_rpt_retail_excellence_summary as (
-    select * from {{ ref('aspedw_integration__edw_id_rpt_retail_excellence_summary') }}
-),
-edw_my_rpt_retail_excellence_summary as (
-    select * from {{ ref('aspedw_integration__edw_my_rpt_retail_excellence_summary') }}
-),
-edw_ph_rpt_retail_excellence_summary as (
-    select * from {{ ref('aspedw_integration__edw_ph_rpt_retail_excellence_summary') }}
-),
-edw_sg_rpt_retail_excellence_summary as (
-    select * from {{ ref('aspedw_integration__edw_sg_rpt_retail_excellence_summary') }}
-),
-edw_kr_rpt_retail_excellence_summary as (
-    select * from {{ ref('aspedw_integration__edw_kr_rpt_retail_excellence_summary') }}
-),
-edw_th_rpt_retail_excellence_summary as (
-    select * from {{ ref('aspedw_integration__edw_th_rpt_retail_excellence_summary') }}
-),
-edw_in_rpt_retail_excellence_summary as (
-    select * from {{ ref('aspedw_integration__edw_in_rpt_retail_excellence_summary') }}
-),
-edw_anz_rpt_retail_excellence_summary as (
-    select * from {{ ref('aspedw_integration__edw_anz_rpt_retail_excellence_summary') }}
-),
-edw_jp_rpt_retail_excellence_summary as (
-    select * from {{ ref('aspedw_integration__edw_jp_rpt_retail_excellence_summary') }}
-),
-edw_cnsc_rpt_retail_excellence_summary as (
-    select * from {{ ref('aspedw_integration__edw_cnsc_rpt_retail_excellence_summary') }}
-),
-edw_cnpc_rpt_retail_excellence_summary as (
-    select * from {{ ref('aspedw_integration__edw_cnpc_rpt_retail_excellence_summary') }}
-),
-
-
-edw_rpt_retail_excellence_summary as 
-(
-SELECT * FROM edw_id_rpt_retail_excellence_summary UNION
-SELECT * FROM edw_my_rpt_retail_excellence_summary UNION
-SELECT * FROM edw_ph_rpt_retail_excellence_summary UNION
-SELECT * FROM edw_kr_rpt_retail_excellence_summary UNION
-SELECT * FROM edw_sg_rpt_retail_excellence_summary UNION
-SELECT * FROM edw_th_rpt_retail_excellence_summary UNION
-SELECT * FROM edw_in_rpt_retail_excellence_summary UNION
-SELECT * FROM edw_anz_rpt_retail_excellence_summary UNION
-SELECT * FROM edw_jp_rpt_retail_excellence_summary UNION
-SELECT * FROM edw_cnsc_rpt_retail_excellence_summary UNION  
-SELECT * FROM edw_cnpc_rpt_retail_excellence_summary
+SELECT * FROM edw_hk_rpt_retail_excellence_summary UNION
+SELECT * FROM edw_tw_rpt_retail_excellence_summary UNION
+SELECT * FROM edw_vn_rpt_retail_excellence_summary
 )
 select * from edw_rpt_retail_excellence_summary
