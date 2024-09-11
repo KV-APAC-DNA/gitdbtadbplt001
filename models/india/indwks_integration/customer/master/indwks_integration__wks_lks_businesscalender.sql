b464962281481969a067033a306727169bd8047c
Asad Patel <apate012@kenvue.com>
2024-09-11 08:40:35 +0000

    final commit

--- models/india/indwks_integration/customer/master/indwks_integration__wks_lks_businesscalender.sql
+++ models/india/indwks_integration/customer/master/indwks_integration__wks_lks_businesscalender.sql
@@ -10,7 +10,8 @@ final as
         year::number(18,0) as year,
         week::varchar(50) as week,
         monthkey::number(18,0) as monthkey,
-        current_timestamp()::timestamp_ntz(9) as updt_dttm
+        current_timestamp()::timestamp_ntz(9) as updt_dttm,
+        file_name:: varchar(225)
     FROM (select distinct * from sdl_lks_businesscalender) src
 )
 select * from final
