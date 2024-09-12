949e0fc8c180ab0d03db64b1d0ce054d9f2fc143
Asad Patel <apate012@kenvue.com>
2024-09-12 07:24:57 +0000

    created separate source file

--- models/india/inditg_integration/csl/master/inditg_integration__itg_csl_retailerhierarchy.sql
+++ models/india/inditg_integration/csl/master/inditg_integration__itg_csl_retailerhierarchy.sql
@@ -30,7 +30,7 @@ final as
         run_id::number(14,0) as run_id,
         current_timestamp()::timestamp_ntz(9) as crt_dttm,
         current_timestamp()::timestamp_ntz(9) as updt_dttm,
-        file_name::varchar(225)
+        file_name::varchar(225) as file_name
         
     from source
 )
