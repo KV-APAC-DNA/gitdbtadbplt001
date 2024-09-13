bd7fffcea6f3edb88edb88d5e2ef3efca55a3917
eswara praneeth Chithathuri <echithat@kenvue.com>
2024-09-13 07:17:50 +0000

    GMC changes

--- models/asp_regional/aspedw_integration/sap_and_sellin/edw/aspedw_integration__v_rpt_copa.sql
+++ models/asp_regional/aspedw_integration/sap_and_sellin/edw/aspedw_integration__v_rpt_copa.sql
@@ -82,7 +82,7 @@ transformed as(
   nvl(gmc.C4_CATEGORY,'Not Available') as CATEGORY,
   nvl(gmc.C5_SUBCATEGORY,'Not Available') as SUBCATEGORY,
 
-  /*IFF(mat.brnd_desc='',null,mat.brnd_desc) as "b2 brand",
+  IFF(mat.brnd_desc='',null,mat.brnd_desc) as "b2 brand",
   IFF(mat.base_prod_desc='',null,mat.base_prod_desc) as "b3 base product",
   IFF(mat.varnt_desc='',null,mat.varnt_desc) as "b4 variant",
   IFF(mat.put_up_desc='',null,mat.put_up_desc) as "b5 put-up",
@@ -91,7 +91,7 @@ transformed as(
   IFF(mat.prodh3_txtmd='',null,mat.prodh3_txtmd) as "prod h3 franchise",
   IFF(mat.prodh4_txtmd='',null,mat.prodh4_txtmd) as "prod h4 product franchise",
   IFF(mat.prodh5_txtmd='',null,mat.prodh5_txtmd) as "prod h5 product major",
-  IFF(mat.prodh6_txtmd='',null,mat.prodh6_txtmd) as "prod h6 product minor",*/
+  IFF(mat.prodh6_txtmd='',null,mat.prodh6_txtmd) as "prod h6 product minor",
   IFF(cus_sales_extn."parent customer"='',null,cus_sales_extn."parent customer") as "parent customer",
   IFF(cus_sales_extn.banner='',null,cus_sales_extn.banner) as banner,
   IFF(cus_sales_extn."banner format"='',null,cus_sales_extn."banner format") as "banner format",
@@ -1511,7 +1511,7 @@ GROUP BY
   nvl(gmc.C4_CATEGORY,'Not Available'),
   nvl(gmc.C5_SUBCATEGORY,'Not Available'), 
 
-  /*IFF(mat.brnd_desc='',null,mat.brnd_desc),
+  IFF(mat.brnd_desc='',null,mat.brnd_desc),
   IFF(mat.base_prod_desc='',null,mat.base_prod_desc),
   IFF(mat.varnt_desc='',null,mat.varnt_desc),
   IFF(mat.put_up_desc='',null,mat.put_up_desc),
@@ -1520,7 +1520,7 @@ GROUP BY
   IFF(mat.prodh3_txtmd='',null,mat.prodh3_txtmd),
   IFF(mat.prodh4_txtmd='',null,mat.prodh4_txtmd),
   IFF(mat.prodh5_txtmd='',null,mat.prodh5_txtmd),
-  IFF(mat.prodh6_txtmd='',null,mat.prodh6_txtmd),*/
+  IFF(mat.prodh6_txtmd='',null,mat.prodh6_txtmd),
   IFF(cus_sales_extn."parent customer"='',null,cus_sales_extn."parent customer"),
   IFF(cus_sales_extn.banner='',null,cus_sales_extn.banner),
   IFF(cus_sales_extn."banner format"='',null,cus_sales_extn."banner format"),
