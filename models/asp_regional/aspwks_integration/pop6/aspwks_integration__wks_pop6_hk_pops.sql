afadab7c5b957c5cd4709d2ced927a487f2c5e6a
Niketh Narasimhan <nnaras01@kenvue.com>
2024-09-16 07:51:54 +0000

    pop6

--- models/asp_regional/aspwks_integration/pop6/aspwks_integration__wks_pop6_hk_pops.sql
+++ models/asp_regional/aspwks_integration/pop6/aspwks_integration__wks_pop6_hk_pops.sql
@@ -2,6 +2,11 @@ with
 source as
 (
     select * from {{ source('ntasdl_raw', 'sdl_pop6_hk_pops') }}
+    where filename not in (
+        select distinct file_name from {{ source('ntawks_integration', 'TRATBL_sdl_pop6_hk_exclusion__lookup_test_1') }}
+        union all
+        select distinct file_name from {{ source('ntawks_integration', 'TRATBL_sdl_pop6_hk_exclusion__lookup_test_2') }}
+    )
 ),
 final as
 (
