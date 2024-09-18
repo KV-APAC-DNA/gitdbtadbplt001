fff38bb3c088880ce355d62d0de5272cbc89dc8d
Debipriya Kanungo <dkanun02@kenvue.com>
2024-09-18 17:39:02 +0000

    fix

--- models/pacific/pcfedw_integration/sales/transactional/pcfedw_integration__edw_demand_forecast_snapshot_temp.sql
+++ models/pacific/pcfedw_integration/sales/transactional/pcfedw_integration__edw_demand_forecast_snapshot_temp.sql
@@ -3,7 +3,7 @@
         materialized="incremental",
         incremental_strategy= "append",
         sql_header="USE WAREHOUSE "+ env_var("DBT_ENV_CORE_DB_MEDIUM_WH")+ ";",
-        cluster_by=[snap_shot_dt,pac_subsource_type,matl_no]
+        cluster_by=['snap_shot_dt','pac_subsource_type','matl_no']
     )
 }}
 
@@ -340,9 +340,9 @@ final as(
     select * from union1
     union all
     select * from union2
-    --{% if is_incremental() %}
+   -- {% if is_incremental() %}
     -- this filter will only be applied on an incremental run
-    --where snap_shot_dt > (select max(snap_shot_dt) from {{ this }}) 
+    -- where snap_shot_dt > (select max(snap_shot_dt) from {{ this }}) 
     --{% endif %}
 )
 select * from final
