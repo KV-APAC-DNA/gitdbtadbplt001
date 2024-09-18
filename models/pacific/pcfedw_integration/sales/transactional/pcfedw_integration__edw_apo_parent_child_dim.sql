ff842a350263c481795fe9d04d40f010488be754
Debipriya Kanungo <dkanun02@kenvue.com>
2024-09-18 05:14:39 +0000

    fix

--- models/pacific/pcfedw_integration/sales/transactional/pcfedw_integration__edw_apo_parent_child_dim.sql
+++
@@ -1,9 +0,0 @@
-with apo_parent_child_dim as 
-(
-    select * from {{ ref('pcfedw_integration__vw_apo_parent_child_dim') }}
-),
-transformed as 
-(
-    select * from apo_parent_child_dim
-)
-select * from transformed
--- models/pacific/pcfedw_integration/sales/transactional/pcfedw_integration__edw_demand_frcst_customer_dim.sql
+++
@@ -1,8 +0,0 @@
-with vw_dmnd_frcst_customer_dim as(
-    select * from {{ ref('pcfedw_integration__vw_dmnd_frcst_customer_dim') }}
-),
-transformered as
-(
-    select * from vw_dmnd_frcst_customer_dim
-)
-select * from transformered
