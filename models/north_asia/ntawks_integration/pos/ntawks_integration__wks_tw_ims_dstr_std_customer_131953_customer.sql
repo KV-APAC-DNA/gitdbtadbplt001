18921ad560eea1db8204477c938960b84776542f
Niketh Narasimhan <nnaras01@kenvue.com>
2024-09-18 00:26:31 +0000

    multifile logic

--- models/north_asia/ntawks_integration/pos/ntawks_integration__wks_tw_ims_dstr_std_customer_131953_customer.sql
+++ models/north_asia/ntawks_integration/pos/ntawks_integration__wks_tw_ims_dstr_std_customer_131953_customer.sql
@@ -18,7 +18,8 @@ final as
 	distributor_contact::varchar(255) as distributor_contact,
 	distributor_sales_area::varchar(20) as distributor_sales_area,
 	current_timestamp()::timestamp_ntz(9) as crt_dttm,
-	current_timestamp()::timestamp_ntz(9) as updt_dttm
+	current_timestamp()::timestamp_ntz(9) as updt_dttm,
+    file_name::varchar(255) as file_name
     from source
 )
 select * from final
