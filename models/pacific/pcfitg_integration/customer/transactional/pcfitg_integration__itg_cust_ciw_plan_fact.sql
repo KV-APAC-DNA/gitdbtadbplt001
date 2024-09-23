52de745ff84a2a9c134373673e0b86dae1c65809
Jeeva Ananth C <jc3@kenvue.com>
2024-09-23 14:37:11 +0000

    renamed temp sdl table

--- models/pacific/pcfitg_integration/customer/transactional/pcfitg_integration__itg_cust_ciw_plan_fact.sql
+++ models/pacific/pcfitg_integration/customer/transactional/pcfitg_integration__itg_cust_ciw_plan_fact.sql
@@ -3,33 +3,13 @@
         materialized="incremental",
         incremental_strategy= "append",
         unique_key=  ['time_period'],
-        pre_hook= "
-        create or replace TABLE PROD_DNA_CORE.DBT_CLOUD_PR_5458_1369.PCFITG_INTEGRATION__ITG_CUST_CIW_PLAN_FACT (
-	TIME_PERIOD VARCHAR(10),
-	SALES_GRP_CD VARCHAR(10),
-	PROD_MJR_CD VARCHAR(30),
-	GOAL_QTY NUMBER(18,0),
-	GOAL_GTS NUMBER(18,2),
-	GOAL_EFF_VAL NUMBER(18,2),
-	GOAL_JGF_SI_VAL NUMBER(18,2),
-	GOAL_PMT_TERMS_VAL NUMBER(18,2),
-	GOAL_DATAINS_VAL NUMBER(18,0),
-	GOAL_EXP_ADJ_VAL NUMBER(18,2),
-	GOAL_JGF_SD_VAL NUMBER(18,2),
-	GOAL_CIW_TOT NUMBER(18,2),
-	GOAL_COGS NUMBER(18,0),
-	GOAL_GP NUMBER(18,2),
-	LOCAL_CCY VARCHAR(10),
-    target_type::varchar(10) as target_type
-);
-        
-        delete from {{this}} where left(time_period, 4) in (
-        select left(time_period, 4) from {{ source('pcfsdl_raw', 'sdl_mds_pacific_cust_ciw_plan_temp') }});"
+        pre_hook= "delete from {{this}} where left(time_period, 4) in (
+        select left(time_period, 4) from {{ source('pcfsdl_raw', 'pacific_cust_ciw_plan_temp') }});"
     )
 }}
 with source as 
 (
-    select * from {{ source('pcfsdl_raw', 'sdl_mds_pacific_cust_ciw_plan_temp') }}
+    select * from {{ source('pcfsdl_raw', 'pacific_cust_ciw_plan_temp') }}
 ),
 final as 
 (
