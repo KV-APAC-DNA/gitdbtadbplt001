110e823f7288b3a4351ee41f30b823fe15a9dac9
Debipriya Kanungo <dkanun02@kenvue.com>
2024-09-26 11:33:41 +0000

    fix

--- models/pacific/pcfedw_integration/forecast/view/pcfedw_integration__vw_demand_forecast_analysis.sql
+++ models/pacific/pcfedw_integration/forecast/view/pcfedw_integration__vw_demand_forecast_analysis.sql
@@ -16,9 +16,13 @@ vw_apo_parent_child_dim as (
 edw_px_forecast_fact as (
     select * from {{ ref('pcfedw_integration__edw_px_forecast_fact') }}
 ),
-vw_sapbw_fact as (
+vw_sapbw_fact_all as (
     select * from {{ ref('pcfedw_integration__vw_sapbw_fact') }}
 ),
+vw_sapbw_fact as (
+    select * from vw_sapbw_fact_all where CIW_CTGRY='GTS'
+),
+
 projprd as (
     select (
             to_char(
