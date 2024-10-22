e643752a6df1b3b26af9065462b7252353512e55
Debipriya Kanungo <dkanun02@kenvue.com>
2024-10-22 04:54:21 +0000

    fix

--- models/philippines/phledw_integration/hcp_sellout_target/phledw_integration__edw_ph_hce_sellout_targets.sql
+++ models/philippines/phledw_integration/hcp_sellout_target/phledw_integration__edw_ph_hce_sellout_targets.sql
@@ -22,8 +22,8 @@ select * from {{ ref('sgpedw_integration__edw_vw_os_time_dim') }}
      target.CUSTOMER_COUNT_TARGET as CUSTOMER_COUNT_TARGET,
      target.district_code,
      prod.team_code,
-     prod.sap_item_code,
-     prod.code as sku 
+    null as sap_item_code,
+    null  as sku 
     from hce_portal_targets target
     inner join (select distinct group_variant_code,team_code,sap_item_code,code from  hce_product_master) prod on (target.GROUP_VARIANT_CODE=prod.GROUP_VARIANT_CODE)
     inner join (select distinct mnth_long,mnth_no,mnth_id,cal_year from edw_vw_os_time_dim ) cal on (cal.mnth_id=target.year_month)
