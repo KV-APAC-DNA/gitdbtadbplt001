c65a646f3af454e7a17b16a0d5d9d2683bd50ce9
eswara praneeth Chithathuri <echithat@kenvue.com>
2024-10-25 05:10:42 +0000

    changes for downstream tables

--- models/indonesia/idnedw_integration/distributor/master/idnedw_integration__edw_distributor_dim.sql
+++ models/indonesia/idnedw_integration/distributor/master/idnedw_integration__edw_distributor_dim.sql
@@ -30,7 +30,9 @@ final as
 	dstrbtr_grp_nm::varchar(255) as dstrbtr_grp_nm,
 	prov_nm::varchar(255) as prov_nm,
 	effective_from::varchar(10) as effective_from,
-	effective_to::varchar(10) as effective_to
+	effective_to::varchar(10) as effective_to,
+    CUSTOMER_SEGMENTATION_LEVEL_1::varchar(500) as CUSTOMER_SEGMENTATION_LEVEL_1,
+    CUSTOMER_SEGMENTATION_LEVEL_2::varchar(500) as CUSTOMER_SEGMENTATION_LEVEL_2 
 from source as idd,
 itg_distributor_province_dim as idpd,
 itg_distributor_group_dim as idgd
