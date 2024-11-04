36df3cb6aee55270483404aa1981534308fbd61d
eswara praneeth Chithathuri <echithat@kenvue.com>
2024-11-04 18:54:01 +0000

    ID downstream changes

---
+++ models/indonesia/idnitg_integration/distributor/master/idnitg_integration__itg_customerpl_segmentation_dim.sql
@@ -0,0 +1,24 @@
+/*{{
+    config
+    (
+        materialized="incremental",
+        incremental_strategy= "append",
+        unique_key= ["code"],
+        pre_hook = "{% if is_incremental() %}
+        delete from {{this}} where (trim(code)) in (select distinct trim(code) from {{ source('idnsdl_raw', 'sdl_mds_id_ref_customerpl_segmentation_adftemp') }});
+        {% endif %}"
+    )
+}}*/
+with source as 
+(
+    select * from {{ source('idnsdl_raw', 'sdl_mds_id_ref_customerpl_segmentation_adftemp') }}
+),
+final as 
+(
+    select
+    trim(code)::varchar(500) as code,
+	trim(customer_segment_level1_code)::varchar(500) as customer_segment_level1_code,
+	trim(customer_segment_level2_code)::varchar(500) as customer_segment_level2_code,
+    from source
+)
+select * from final
