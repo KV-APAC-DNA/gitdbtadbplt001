4c4011758b1c20759912264fbde752eb75d78586
eswara praneeth Chithathuri <echithat@kenvue.com>
2024-11-06 06:31:53 +0000

    ID changes for PL

--- models/indonesia/idnitg_integration/distributor/idnitg_integration__itg_customerpl_segmentation_dim.sql
+++ models/indonesia/idnitg_integration/distributor/idnitg_integration__itg_customerpl_segmentation_dim.sql
@@ -1,3 +1,14 @@
+{{
+    config
+    (
+        materialized="incremental",
+        incremental_strategy= "append",
+        unique_key= ["code"],
+        pre_hook = "{% if is_incremental() %}
+        delete from {{this}} where (trim(code)) in (select distinct trim(code) from {{ source('idnsdl_raw', 'sdl_mds_id_ref_customerpl_segmentation_adftemp') }});
+        {% endif %}"
+    )
+}}
 with source as 
 (
     select * from {{ source('idnsdl_raw', 'sdl_mds_id_ref_customerpl_segmentation_adftemp') }}
