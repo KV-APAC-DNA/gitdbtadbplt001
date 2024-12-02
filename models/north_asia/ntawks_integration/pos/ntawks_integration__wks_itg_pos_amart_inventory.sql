cc6d796c535dd5db1134c288fc59baa16a8913ea
Jeeva Ananth C <jc3@kenvue.com>
2024-12-02 05:53:27 +0000

    added pre hook to delete amart value

--- models/north_asia/ntawks_integration/pos/ntawks_integration__wks_itg_pos_amart_inventory.sql
+++ models/north_asia/ntawks_integration/pos/ntawks_integration__wks_itg_pos_amart_inventory.sql
@@ -1,7 +1,9 @@
+--remove pre_hhok during deployment
 {{
     config(
         materialized='incremental',
-        incincremental_strategy='append'
+        incincremental_strategy='append',
+        pre_hook = "delete from prod_dna_core.dbt_cloud_pr_5458_1580.ntaitg_integration__itg_pos where src_sys_cd like 'A-Mart%';"
     )
 }}
 
