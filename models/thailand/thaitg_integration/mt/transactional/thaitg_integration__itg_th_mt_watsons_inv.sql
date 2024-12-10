7a84c32547b0aa5fc76d3625df5595d585ff1775
Manjusha Madhu <mmadhu@kenvue.com>
2024-12-10 13:40:33 +0000

    Correct SDL table name for watsons inv

--- models/thailand/thaitg_integration/mt/transactional/thaitg_integration__itg_th_mt_watsons_inv.sql
+++ models/thailand/thaitg_integration/mt/transactional/thaitg_integration__itg_th_mt_watsons_inv.sql
@@ -5,7 +5,7 @@
     )}}
 
 with source as(
-    select * from {{ source('thasdl_raw', 'sdl_th_mt_watsons_1209_UAT') }}
+    select * from {{ source('thasdl_raw', 'sdl_th_mt_watson_1209_UAT') }}
 ),
 final as(
     select * from source
