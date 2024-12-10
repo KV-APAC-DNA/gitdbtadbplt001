3220b660bf0dbedf4930d032f5f2ec055a6bfcd9
Manjusha Madhu <mmadhu@kenvue.com>
2024-12-10 09:50:39 +0000

    testing

--- models/india/inditg_integration/lidar/flat_file_trd_med/inditg_integration__fct_tv_grp_spends_global_monthly.sql
+++ models/india/inditg_integration/lidar/flat_file_trd_med/inditg_integration__fct_tv_grp_spends_global_monthly.sql
@@ -2,7 +2,6 @@
    materialized='incremental',
   incremental_strategy='append',
   transient=false,
-  post_hook="{{ get_harmonized_brand('TRADITIONAL_COMPETITIVE_TV_GRP_SPENDS', 'Y') }}"
 ) }}
 
 select
