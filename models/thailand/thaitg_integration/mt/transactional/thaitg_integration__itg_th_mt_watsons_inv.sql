203f28893f86819861cf2e0ba2ab32d859d71d26
Manjusha Madhu <mmadhu@kenvue.com>
2024-12-10 10:43:15 +0000

    Added UAT source table to the model for Watsons inventory

--- models/thailand/thaitg_integration/mt/transactional/thaitg_integration__itg_th_mt_watsons_inv.sql
+++ models/thailand/thaitg_integration/mt/transactional/thaitg_integration__itg_th_mt_watsons_inv.sql
@@ -5,7 +5,7 @@
     )}}
 
 with source as(
-    select * from {{ source('thasdl_raw', 'sdl_th_mt_watsons') }}
+    select * from {{ source('thasdl_raw', 'sdl_th_mt_watsons_1209_UAT') }}
 ),
 final as(
     select * from source
