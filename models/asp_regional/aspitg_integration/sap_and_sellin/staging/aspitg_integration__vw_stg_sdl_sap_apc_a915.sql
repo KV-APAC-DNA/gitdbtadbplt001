5814ffa1a49298e417f0b554f7556cca837ad9d4
Jeeva Ananth C <jc3@kenvue.com>
2024-12-12 07:56:35 +0000

    added models for A930, A915 tables

---
+++ models/asp_regional/aspitg_integration/sap_and_sellin/staging/aspitg_integration__vw_stg_sdl_sap_apc_a915.sql
@@ -0,0 +1,28 @@
+
+with source as(
+    select * from {{ source('apc_access', 'apc_a915') }}
+),
+final as(
+    select
+        mandt as mandt,
+        kappl as kappl,
+        kschl as kschl,
+        vkorg as vkorg,
+        matnr as matnr,
+        datbi as datbi,
+        datab as datab,
+        knumh as knumh,
+        -- _deleted_ as _deleted_,
+        -- _pk_ as _pk_,
+        _createtime_::timestamp_ntz(9) as crt_dttm,
+        -- _kafkaoffset_ as _kafkaoffset_,
+        -- _upt_ as _upt_,
+        -- _kafkatimestamp_ as _kafkatimestamp_,
+        -- _infa_bigint_sequence_ as _infa_bigint_sequence_,
+        pk_md5 as pk_md5,
+        null as file_name
+    from source
+    where _deleted_='F'
+
+)
+select * from final
