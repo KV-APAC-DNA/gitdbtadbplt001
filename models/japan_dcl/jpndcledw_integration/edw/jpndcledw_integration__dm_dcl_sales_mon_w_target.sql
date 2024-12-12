90979a0637549ad9c7062dd911eb79cacae4c7bb
wdenys01@kenvue.com <wdenys01@kenvue.com>
2024-12-12 18:28:34 +0000

    added logic to translate channel from japanese to english for 360 view dashboard

--- models/japan_dcl/jpndcledw_integration/edw/jpndcledw_integration__dm_dcl_sales_mon_w_target.sql
+++ models/japan_dcl/jpndcledw_integration/edw/jpndcledw_integration__dm_dcl_sales_mon_w_target.sql
@@ -11,24 +11,33 @@ date_mapping AS (
     SELECT *
     FROM {{ ref('jpndcledw_access__cld_m') }}  -- Reference to 445 period
 ),
+translated_channels AS (
+    SELECT *,
+        CASE
+            WHEN "channel" = '通販' THEN 'E-Commerce'
+            WHEN "channel" = '直営・百貨店' THEN 'Direct Store & Department Store'
+            ELSE "channel"  -- Keep original value if no match
+        END AS channel_english
+    FROM d2c_data
+),
 d2c_monthly as (
-SELECT 
-    LEFT(Y."ymonth_445", 4) || SUBSTR(Y."ymonth_445", 6, 2) AS month_id,
-    SUM(K."gts") AS gts_base,
-    LAG(SUM(K."gts"), 1, 0) OVER (PARTITION BY K."channel" ORDER BY month_id) AS gts_mom,
-    LAG(SUM(K."gts"), 12, 0) OVER (PARTITION BY K."channel" ORDER BY month_id) AS gts_yoy,
-    SUM(K."nts") AS nts_base,
-    LAG(SUM(K."nts"), 1, 0) OVER (PARTITION BY K."channel" ORDER BY month_id) AS nts_mom,
-    LAG(SUM(K."nts"), 12, 0) OVER (PARTITION BY K."channel" ORDER BY month_id) AS nts_yoy,
-    K."channel" AS channel,
-    (UNIFORM(0::FLOAT, 1.5::FLOAT, RANDOM())) * SUM(K."gts") AS gts_target,
-    (UNIFORM(0::FLOAT, 1.5::FLOAT, RANDOM())) * SUM(K."nts") AS nts_target
-FROM d2c_data K
-LEFT JOIN date_mapping Y ON K."order_dt" = Y."ymd_dt"
-WHERE 
-    K."order_dt" >= to_char(extract(year FROM dateadd(year, -3, current_date)))  || '-01-01' 
-    AND K."channel" IN ('Web', '通販', '直営・百貨店')
-GROUP BY month_id, channel
+    SELECT 
+        LEFT(Y."ymonth_445", 4) || SUBSTR(Y."ymonth_445", 6, 2) AS month_id,
+        SUM(K."gts") AS gts_base,
+        LAG(SUM(K."gts"), 1, 0) OVER (PARTITION BY K.channel_english ORDER BY month_id) AS gts_mom,
+        LAG(SUM(K."gts"), 12, 0) OVER (PARTITION BY K.channel_english ORDER BY month_id) AS gts_yoy,
+        SUM(K."nts") AS nts_base,
+        LAG(SUM(K."nts"), 1, 0) OVER (PARTITION BY K.channel_english ORDER BY month_id) AS nts_mom,
+        LAG(SUM(K."nts"), 12, 0) OVER (PARTITION BY K.channel_english ORDER BY month_id) AS nts_yoy,
+        K.channel_english AS channel,
+        (UNIFORM(0::FLOAT, 1.5::FLOAT, RANDOM())) * SUM(K."gts") AS gts_target,
+        (UNIFORM(0::FLOAT, 1.5::FLOAT, RANDOM())) * SUM(K."nts") AS nts_target
+    FROM translated_channels K
+    LEFT JOIN date_mapping Y ON K."order_dt" = Y."ymd_dt"
+    WHERE 
+        K."order_dt" >= to_char(extract(year FROM dateadd(year, -3, current_date))) || '-01-01' 
+        AND K.channel_english IN ('Web', 'E-Commerce', 'Direct Store & Department Store')
+    GROUP BY month_id, channel
 ), 
 Non_d2c_data as (
     SELECT *
@@ -36,29 +45,27 @@ Non_d2c_data as (
 ),
 Non_d2c_monthly as (
     SELECT extract(year from K."fisc_day") || substr(to_char(K."fisc_day"),6,2) as month_id,
-    sum(K."gts_lcy") AS gts_base, 
-    LAG(SUM(K."gts_lcy"), 1, 0) OVER (PARTITION BY K."channel" ORDER BY month_id) AS gts_mom,
-    LAG(SUM(K."gts_lcy"), 12, 0) OVER (PARTITION BY K."channel" ORDER BY month_id) AS gts_yoy,
-    SUM(K."nts_lcy") AS nts_base,
-    LAG(SUM(K."nts_lcy"), 1, 0) OVER (PARTITION BY K."channel" ORDER BY month_id) AS nts_mom,
-    LAG(SUM(K."nts_lcy"), 12, 0) OVER (PARTITION BY K."channel" ORDER BY month_id) AS nts_yoy, 
-    K."channel" AS channel,
-    (UNIFORM(0.5::FLOAT, 1.5::FLOAT, RANDOM())) * SUM(K."gts_lcy") AS gts_target,
-    (UNIFORM(0.5::FLOAT, 1.5::FLOAT, RANDOM())) * SUM(K."nts_lcy") AS nts_target
+        SUM(K."gts_lcy") AS gts_base, 
+        LAG(SUM(K."gts_lcy"), 1, 0) OVER (PARTITION BY K."channel" ORDER BY month_id) AS gts_mom,
+        LAG(SUM(K."gts_lcy"), 12, 0) OVER (PARTITION BY K."channel" ORDER BY month_id) AS gts_yoy,
+        SUM(K."nts_lcy") AS nts_base,
+        LAG(SUM(K."nts_lcy"), 1, 0) OVER (PARTITION BY K."channel" ORDER BY month_id) AS nts_mom,
+        LAG(SUM(K."nts_lcy"), 12, 0) OVER (PARTITION BY K."channel" ORDER BY month_id) AS nts_yoy, 
+        K."channel" AS channel,
+        (UNIFORM(0.5::FLOAT, 1.5::FLOAT, RANDOM())) * SUM(K."gts_lcy") AS gts_target,
+        (UNIFORM(0.5::FLOAT, 1.5::FLOAT, RANDOM())) * SUM(K."nts_lcy") AS nts_target
     FROM Non_d2c_data K
-    where K."brand" = 'Dr. Ci: Labo' and K."ctry_nm" = 'Japan DCL'
-    and channel in ('E-Commerce', 'Wholesaler')
-    group by month_id, channel
-), 
-FINAL AS
-(
-    SELECT * from d2c_monthly
-    UNION
-    SELECT * FROM Non_d2c_monthly
+    WHERE 
+        K."fisc_day" >= to_char(extract(year FROM dateadd(year, -3, current_date))) || '-01-01'
+        AND K."channel" IN ('Web', 'E-Commerce', 'Direct Store & Department Store')
+    GROUP BY month_id, K."channel"
+),
+final_data as (
+    SELECT * 
+    FROM d2c_monthly
+    UNION ALL
+    SELECT * 
+    FROM Non_d2c_monthly
 )
-SELECT *, 
-current_timestamp()::timestamp_ntz(9) as inserted_date,
-null::varchar(100) as inserted_by ,
-current_timestamp()::timestamp_ntz(9) as updated_date,
-null::varchar(100) as updated_by
-FROM FINAL
+SELECT * 
+FROM final_data
