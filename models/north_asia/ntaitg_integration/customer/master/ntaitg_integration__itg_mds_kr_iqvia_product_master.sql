5e6abe3c374eca20da9e9c7f5e0a0a9d41439068
Jeeva Ananth C <jc3@kenvue.com>
2024-12-16 05:54:42 +0000

    initial commit

---
+++ models/north_asia/ntaitg_integration/customer/master/ntaitg_integration__itg_mds_kr_iqvia_product_master.sql
@@ -0,0 +1,15 @@
+
+with source as (
+    select * from {{ source('ntasdl_raw', 'sdl_mds_kr_iqvia_product_master_adftemp') }}
+),
+final as (
+    select
+	PRODUCTNAME,
+	FORM_DESCRIPTION,
+	PACKSIZE,
+	CHC4_DESC,
+	EAN
+    from source
+)
+
+select * from final
---
+++ models/north_asia/ntaitg_integration/product/transactional/ntaitg_integration__itg_kr_iqvia_offtake_history.sql
@@ -0,0 +1,48 @@
+
+
+with SDL_KR_IQVIA_OFFTAKE as (
+    select * from {{ source('ntasdl_raw', 'sdl_kr_iqvia_offtake_temp') }}
+),
+transformed as (
+    SELECT  
+ 	AUDIT_CODE,
+	DATA_PERIOD,
+	PERIOD_YYYYMM,
+	PRODUCT_NAME,
+	PRODUCT_NAME_KOR,
+	FORM_DESCRIPTION,
+	PACK_SIZE,
+	MOLECULE_DESC,
+	MFR_NAME,
+	MFR_NAME_KOR,
+	MFR_TYPE,
+	NHI_TYPE,
+	CHC_1_CODE,
+	CHC_1_DESC,
+	CHC_2_CODE,
+	CHC_2_DESC,
+	CHC_3_CODE,
+	CHC_3_DESC,
+	CHC_4_CODE,
+	CHC_4_DESC,
+	ATC_1_CODE,
+	ATC_1_DESC,
+	ATC_2_CODE,
+	ATC_2_DESC,
+	ATC_3_CODE,
+	ATC_3_DESC,
+	ATC_4_CODE,
+	ATC_4_DESC,
+	UNITS,
+	PRICE,
+	VALUES_LC_SI_PRICE,
+	VALUES_LC_SO_PRICE,
+	FILE_NAME,
+	CRTD_DTTM
+FROM SDL_KR_IQVIA_OFFTAKE 
+),
+
+final as (
+    from transformed
+)
+select * from final
